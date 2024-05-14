0a762977823fbdfa0158493d5022640d2c7ae25f
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-14 12:36:05 +0530

    binary to decimal

--- src/JAVA_M1/BinaryToDecimal.java
+++ src/JAVA_M1/BinaryToDecimal.java
@@ -1,18 +1,26 @@
 package JAVA_M1;
 public class BinaryToDecimal {
     public int BtoD(String b){
-//        int conv = 1;
-//        int result = 0;
-//        for (int i = 1; i <= b.length(); i++){
-//            if(b.charAt(b.length()-i)) == '1'){
-//                result += conv;
-//            }
-//            conv *= 2;
-//        }
-//        return result;
-        return 0;
+        int conv = 1;
+        int result = 0;
+        for (int i = 1; i <= b.length(); i++){
+            if(b.charAt(b.length()-i) == '1')
+                result += conv;
+            conv *= 2;
+        }
+        return result;
+    }
+    public int BtoDAlt(String b){
+        int res = 0;
+        for (int i =1; i<=b.length();i++){
+            if (b.charAt(b.length()-i)=='1')
+                res += Math.pow(2, i-1);
+        }
+        return res;
     }
     public static void main(String[] args){
-        System.out.println("hi");
+        BinaryToDecimal binaryToDecimal = new BinaryToDecimal();
+        System.out.println(binaryToDecimal.BtoD("10110"));
+        System.out.println(binaryToDecimal.BtoDAlt("10110"));
     }
 }
