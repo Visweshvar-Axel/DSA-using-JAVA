169237ffbc8f6201af011888e260ce7a81f76053
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-14 12:16:54 +0530

    bubble sort

---
+++ src/BubbleSort/BubbleSort.java
@@ -0,0 +1,54 @@
+package BubbleSort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public void swap(int[] num,int i,int j){
+        int temp=num[i];
+        num[i] = num[j];
+        num[j] = temp;
+    }
+    public void sort(int[] num){
+        for(int i=0;i<num.length;i++){
+            for(int j=i;j<num.length;j++){
+                if(num[j]>num[j+1]){
+                    int temp=num[i];
+                    num[i] = num[j];
+                    num[j] = temp;
+                }
+            }
+        }
+    }
+    public void sBubbleSort(int[] num){
+        for(int i=0;i<num.length;i++){
+            for(int j=i;j<num.length;j++){
+                if(num[j]>num[j+1]){
+                    swap(num, j,j+1);
+                }
+            }
+        }
+    }
+    public void sortImproved(int[] num){
+        int i=0;
+        boolean swapOccured = true;
+        while (swapOccured){
+            swapOccured = false;
+            for (int j=0;j< num.length-i;j++){
+                swap(num, j, j+1);
+                swapOccured = true;
+            }
+        }
+    }
+    public static void main(String[] args){
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] num = new int[]{2,5,7,2,4,2,8,1,0};
+        int[] num1 = new int[]{2,5,7,2,4,2,8,1,0};
+        int[] num2 = new int[]{2,5,7,2,4,2,8,1,0};
+        bubbleSort.sort(num);
+        bubbleSort.sBubbleSort(num1);
+        bubbleSort.sortImproved(num2);
+        System.out.println(Arrays.toString(num));
+        System.out.println(Arrays.toString(num));
+        System.out.println(Arrays.toString(num));
+    }
+}
---
+++ src/JAVA_M1/BinaryToDecimal.java
@@ -0,0 +1,18 @@
+package JAVA_M1;
+public class BinaryToDecimal {
+    public int BtoD(String b){
+//        int conv = 1;
+//        int result = 0;
+//        for (int i = 1; i <= b.length(); i++){
+//            if(b.charAt(b.length()-i)) == '1'){
+//                result += conv;
+//            }
+//            conv *= 2;
+//        }
+//        return result;
+        return 0;
+    }
+    public static void main(String[] args){
+        System.out.println("hi");
+    }
+}
