54bed8650ac0a9fbe240ed6b2949dcb22e69323c
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-14 13:52:15 +0530

    binary search

--- src/lesson2/Sorting/BinarySearch.java
+++ src/lesson2/Sorting/BinarySearch.java
@@ -6,7 +6,17 @@ public class BinarySearch {
     }
     public boolean binarySearch(int x, int[] num, int beg,int end){
         if(beg<=end){
-            int mid=(end-beg)
+            int mid=(end-beg) /2 + beg;
+            if (num[mid]==x) return true;
+            if (num[mid]>x) return binarySearch(x,num,beg,mid-1);
+            return binarySearch(x,num,mid+1,end);
         }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        BinarySearch binarySearch = new BinarySearch();
+        System.out.println(binarySearch.binarySearch(7,new int[]{1,2,3,4,5,6,7,8,9,10}));
+        System.out.println(binarySearch.binarySearch(0,new int[]{1,2,3,4,5,6,7,8,9,10}));
     }
 }
