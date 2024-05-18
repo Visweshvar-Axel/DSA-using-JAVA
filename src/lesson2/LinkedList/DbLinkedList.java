542e8457e85152e7585901ed2ffe6361590c00d2
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-18 09:45:30 +0530

    DbLinkedList imp-

--- src/lesson2/LinkedList/DbLinkedList.java
+++ src/lesson2/LinkedList/DbLinkedList.java
@@ -1,4 +1,38 @@
 package lesson2.LinkedList;
 
-public class DbLinkedList {
+import java.util.Optional;
+
+public class DbLinkedList<V> {
+    private V value;
+    private DbLinkedList<V> next;
+    private DbLinkedList<V> previous;
+
+    public DbLinkedList(V value,DbLinkedList<V> previous, DbLinkedList<V> next){
+        this.value = value;
+        this.previous = previous;
+        this.next = next;
+    }
+
+    public Optional<DbLinkedList<V>> getNext(){return Optional.ofNullable(next);}
+    public Optional<DbLinkedList<V>> getPrevious(){return Optional.ofNullable(previous);}
+
+    public V getValue() {return value;}
+    public DbLinkedList<V> setValue(V value) {
+        this.value = value;
+        return this;
+    }
+
+    public DbLinkedList<V> setNext(DbLinkedList<V> next) {
+        this.next = next;
+        return this;
+    }
+
+    public DbLinkedList<V> setPrevious(DbLinkedList<V> previous) {
+        this.previous = previous;
+        return this;
+    }
+    @Override
+    public String toString() {
+        return "LinkedListNode{value="+value+"}";
+    }
 }
