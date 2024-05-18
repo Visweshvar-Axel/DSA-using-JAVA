4820fde63db6a873094f7e7c779b1b26386a5aed
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-18 10:22:18 +0530

    Queue +

---
+++ src/lesson2/queue/Queue.java
@@ -0,0 +1,37 @@
+package lesson2.queue;
+
+import lesson2.LinkedList.DbLinkedList;
+
+import java.util.Optional;
+
+public class Queue<V> {
+    private DbLinkedList<V> head;
+    private DbLinkedList<V> tail;
+    private Queue(){
+        head = null;
+        tail = null;
+    }
+    public void enqueue(V item){
+        DbLinkedList<V> node = new DbLinkedList<V>(item, tail, null);
+        Optional.ofNullable(tail).ifPresent(n -> n.setNext(node));
+        tail = node;
+        if (head == null) head = node;
+    }
+    public Optional<V> dequeue(){
+        Optional<DbLinkedList<V>> node = Optional.ofNullable(head);
+        head = node.flatMap(DbLinkedList::getNext).orElse(null);
+        Optional.ofNullable(head).ifPresent(n -> n.setPrevious(null));
+        if (head == null) tail = null;
+        return node.map(DbLinkedList::getValue);
+    }
+
+    public static void main(String[] args) {
+        Queue<String> q = new Queue<String>();
+        q.enqueue("Viswa");
+        q.enqueue("axel");
+        q.enqueue("jou");
+        q.enqueue("full");
+
+
+    }
+}
