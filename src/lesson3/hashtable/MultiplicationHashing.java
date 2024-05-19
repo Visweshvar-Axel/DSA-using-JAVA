884b4754090bf6aa663e3612978780e55cef4820
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-19 22:24:27 +0530

    MultiplicationHashing imp-

--- src/lesson3/hashtable/MultiplicationHashing.java
+++ src/lesson3/hashtable/MultiplicationHashing.java
@@ -1,5 +1,15 @@
 package lesson3.hashtable;
 
 public class MultiplicationHashing {
+    private Double k;
+    public MultiplicationHashing(Double k){ this.k = k;}
+    public int hashKey(Integer key, int tableSize){return (int) (tableSize * (k * key % 1));}
 
+    public static void main(String[] args) {
+        MultiplicationHashing multiplicationHashing = new MultiplicationHashing((Math.sqrt(5)-1)/2);
+        System.out.println(multiplicationHashing.hashKey(12342342,1000));
+        System.out.println(multiplicationHashing.hashKey(23545646,1000));
+        System.out.println(multiplicationHashing.hashKey(87934653,1000));
+        System.out.println(multiplicationHashing.hashKey(25464566,1000));
+    }
 }
