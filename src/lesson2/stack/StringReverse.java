be08ed64d27c19d25850cd75de76cc1170f4b490
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-19 19:43:31 +0530

    StringReverse imp-

--- src/lesson2/stack/StringReverse.java
+++ src/lesson2/stack/StringReverse.java
@@ -3,5 +3,20 @@ package lesson2.stack;
 import java.util.Optional;
 
 public class StringReverse {
-
+    public String reverse(String str){
+        StringBuffer res = new StringBuffer();
+        Stack<Character> stack = new Stack<>();
+        for(char c : str.toCharArray()){
+            stack.push(c);
+        }
+        Optional<Character> optchar = stack.pop();
+        while (optchar.isPresent()){
+            res.append(optchar.get());
+            optchar = stack.pop();
+        }
+        return res.toString();
+    }
+    public static void main(String[] args) {
+        System.out.println(new StringReverse().reverse("This will be reversed"));
+    }
 }
