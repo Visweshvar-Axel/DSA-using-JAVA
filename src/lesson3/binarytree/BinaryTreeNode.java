f5df9daa9a878c842c4cc1d49627f1c73416bc9a
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-20 02:21:54 +0530

    BinaryTreeNode imp-

--- src/lesson3/binarytree/BinaryTreeNode.java
+++ src/lesson3/binarytree/BinaryTreeNode.java
@@ -1,4 +1,30 @@
 package lesson3.binarytree;
 
-public class BinaryTreeNode {
+import java.util.Optional;
+
+public class BinaryTreeNode<K, V> {
+    private BinaryTreeNode<K, V> left;
+    private BinaryTreeNode<K, V> right;
+    private K key;
+    private V value;
+
+    public BinaryTreeNode(K key, V value){
+        this.key = key;
+        this.value = value;
+    }
+
+    public Optional<BinaryTreeNode<K, V>> getLeft() {return Optional.ofNullable(left);}
+    public Optional<BinaryTreeNode<K, V>> getRight() {return Optional.ofNullable(right);}
+
+    public void setLeft(BinaryTreeNode<K, V> left) {this.left = left;}
+
+    public void setRight(BinaryTreeNode<K, V> right) {this.right = right;}
+
+    public K getKey() {return key;}
+
+    public V getValue() {return value;}
+
+    public void setValue(V value) {this.value = value;}
+
+    public void setKey(K key) {this.key = key;}
 }
