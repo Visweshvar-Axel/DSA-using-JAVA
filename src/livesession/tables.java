5320ebb333056391680dcfcbfce3bdaa50ecbf9c
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-21 17:46:15 +0530

    tables imp+

--- src/livesession/tables.java
+++ src/livesession/tables.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class tables {
     public static void main(String[] args) {
-
+        int t;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a table number: ");
+        t =sc.nextInt();
+        for(int i = 1;i <=10;i++) System.out.println(t+" x "+i+" = "+(t*i));
     }
 }
