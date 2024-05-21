d9204b1db873821eb28370e7f87d98434b6d2e3e
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-21 17:37:49 +0530

    Palindrome imp+

--- src/livesession/Palindrome.java
+++ src/livesession/Palindrome.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string");
         String inp = sc.nextLine();
         String rev = "";
         for(char c: inp.toCharArray()){
             rev = c + rev;
         }
-        if(in)
+        if(inp.equals(rev)) System.out.println("the given string is palindrome");
+        else System.out.println("the given string is not a palindrome");
     }
 }
