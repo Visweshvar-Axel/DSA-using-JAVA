b27848c729e96e5c98b47db3768be95971843ba8
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-22 20:28:07 +0530

    lexo 2 string+

--- src/livesession/lexo.java
+++ src/livesession/lexo.java
@@ -2,6 +2,14 @@ package livesession;
 
 public class lexo {
     public static void main(String[] args) {
-
+        int alp[] = new int[26];
+        String str1 = "listen";
+        String str2 = "silent";
+        for(int i=0;i<str1.length();i++){
+            alp[(int) str1.indexOf(i)]++;
+        }
+        for(int i=0;i<str2.length();i++){
+            alp[(int) str2.indexOf(i)]--;
+        }
     }
 }
