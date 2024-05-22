0622ec07c78f1912bd9c7821618cd6993536b325
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-22 20:31:10 +0530

    lexo -1 bound+

--- src/livesession/lexo.java
+++ src/livesession/lexo.java
@@ -5,14 +5,14 @@ public class lexo {
         int alp[] = new int[26];
         String str1 = "listen";
         String str2 = "silent";
-        for(int i=0;i<str1.length();i++){
+        for(int i=0;i<str1.length()-1;i++){
             alp[(int) str1.indexOf(i)]++;
         }
-        for(int i=0;i<str2.length();i++){
+        for(int i=0;i<str2.length()-1;i++){
             alp[(int) str2.indexOf(i)]--;
         }
         int sum=0;
-        for(int i=0;i<26;i++){
+        for(int i=0;i<25;i++){
             sum+=alp[i];
         }
         if(sum!=0) System.out.println("yes");
