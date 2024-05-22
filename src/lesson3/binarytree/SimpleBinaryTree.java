558a5fe45db1d567ed58d896d4c67a4e0b3a8422
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-22 18:07:30 +0530

    SimpleBinaryTree comment

--- src/lesson3/binarytree/SimpleBinaryTree.java
+++ src/lesson3/binarytree/SimpleBinaryTree.java
@@ -1,25 +1,26 @@
 package lesson3.binarytree;
 
-public class SimpleBinaryTree<K, V> implements BinaryTree<K, V>{
-    protected BinaryTreeNode<K, V> root;
-    public void put(K key,V value){
-        if(root == null) root = new BinaryTreeNode<>(key,value);
-        else put(key,value,root);
-    }
-    public void put(K key,V value,BinaryTreeNode<K,V> node){
-        if (((Comparable) key).compareTo(node.getKey())==0){
-            node.setKey(key);
-            node.setValue(value);
-        } else if (((Comparable) key).compareTo(node.getKey()) < 0){
-            if (node.getLeft().isPresent())
-                put(key,value,node.getLeft().get());
-            else
-                node.setLeft(new BinaryTreeNode<>(key,value));
-        } else {
-            if (node.getRight().isPresent())
-                put(key,value,node.getRight().get());
-            else
-                node.setRight(new BinaryTreeNode<>(key, value));
-        }
-    }
+public class SimpleBinaryTree{
+//public class SimpleBinaryTree<K, V> implements BinaryTree<K, V>{
+//    protected BinaryTreeNode<K, V> root;
+//    public void put(K key,V value){
+//        if(root == null) root = new BinaryTreeNode<>(key,value);
+//        else put(key,value,root);
+//    }
+//    public void put(K key,V value,BinaryTreeNode<K,V> node){
+//        if (((Comparable) key).compareTo(node.getKey())==0){
+//            node.setKey(key);
+//            node.setValue(value);
+//        } else if (((Comparable) key).compareTo(node.getKey()) < 0){
+//            if (node.getLeft().isPresent())
+//                put(key,value,node.getLeft().get());
+//            else
+//                node.setLeft(new BinaryTreeNode<>(key,value));
+//        } else {
+//            if (node.getRight().isPresent())
+//                put(key,value,node.getRight().get());
+//            else
+//                node.setRight(new BinaryTreeNode<>(key, value));
+//        }
+//    }
 }
