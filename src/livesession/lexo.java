c5bb1fe1f466ba3ea7d81d4b52f5445c9772ce1c
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-22 20:31:38 +0530

    lexo type casting+

--- src/livesession/lexo.java
+++ src/livesession/lexo.java
@@ -5,14 +5,14 @@ public class lexo {
         int alp[] = new int[26];
         String str1 = "listen";
         String str2 = "silent";
-        for(int i=0;i<str1.length()-1;i++){
-            alp[(int) str1.indexOf(i)]++;
+        for(int i=0;i<str1.length();i++){
+            alp[Integer.parseInt(str1.charAt(i)-1)]++;
         }
-        for(int i=0;i<str2.length()-1;i++){
-            alp[(int) str2.indexOf(i)]--;
+        for(int i=0;i<str2.length();i++){
+            alp[(int) str2.charAt(i)-1]--;
         }
         int sum=0;
-        for(int i=0;i<25;i++){
+        for(int i=0;i<24;i++){
             sum+=alp[i];
         }
         if(sum!=0) System.out.println("yes");
