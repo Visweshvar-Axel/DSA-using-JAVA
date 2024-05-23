d41fab12ece85174f992cefed1a3bd7fde46fa3a
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-23 20:28:35 +0530

    frequent maxc+

--- src/livesession/array/frequent.java
+++ src/livesession/array/frequent.java
@@ -5,6 +5,7 @@ public class frequent {
         int [] marks = {1,2,3,44,5,5,6,7,7,8,88,8,8};
         int n = marks.length-1;
         int max = 0;
+        int maxc = 0;
         for (int i = 0; i<=n;i++){
             int c = 0;
             for (int j = 0;j<=n;j++){
