88abab87378ce3a3e5722b4528db7c622dbe4038
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-23 20:24:22 +0530

    frequent +

---
+++ src/livesession/array/frequent.java
@@ -0,0 +1,9 @@
+package livesession.array;
+
+public class frequent {
+    public static void main(String[] args) {
+        int [] marks = {1,2,3,44,5,5,6,7,7,8,88,8,8};
+        int n = marks.length-1;
+        for ()
+    }
+}
