fcbb335c6bdd1796b78694d07603ba91fa40842b
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-23 20:26:37 +0530

    frequent for j+

--- src/livesession/array/frequent.java
+++ src/livesession/array/frequent.java
@@ -4,6 +4,8 @@ public class frequent {
     public static void main(String[] args) {
         int [] marks = {1,2,3,44,5,5,6,7,7,8,88,8,8};
         int n = marks.length-1;
-        for (int i = 0; i<=n;i++){}
+        for (int i = 0; i<=n;i++){
+            for (int j = 0;j<=n;j++){}
+        }
     }
 }
