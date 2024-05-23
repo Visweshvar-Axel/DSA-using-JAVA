69b93d2e5a32b939790aa5df72418c0df596b9cc
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-23 20:30:27 +0530

    frequent if2+

--- src/livesession/array/frequent.java
+++ src/livesession/array/frequent.java
@@ -2,6 +2,7 @@ package livesession.array;
 
 public class frequent {
     public static void main(String[] args) {
+//        int [] marks = {1,2,3,44,5,5,6,7,7,8,88,8,8};
         int [] marks = {1,2,3,44,5,5,6,7,7,8,88,8,8};
         int n = marks.length-1;
         int max = marks[0];
