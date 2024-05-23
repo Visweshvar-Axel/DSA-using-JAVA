27657ba59a9377f086522a489f1ad99a77cdb305
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-23 20:27:00 +0530

    frequent if+

--- src/livesession/array/frequent.java
+++ src/livesession/array/frequent.java
@@ -5,7 +5,9 @@ public class frequent {
         int [] marks = {1,2,3,44,5,5,6,7,7,8,88,8,8};
         int n = marks.length-1;
         for (int i = 0; i<=n;i++){
-            for (int j = 0;j<=n;j++){}
+            for (int j = 0;j<=n;j++){
+                if(i != j)
+            }
         }
     }
 }
