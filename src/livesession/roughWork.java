672afb1ff6484c15687b3cf5e7470b4788801a28
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-26 21:53:21 +0530

    roughWork

---
+++ src/livesession/roughWork.java
@@ -0,0 +1,14 @@
+package livesession;
+
+import java.util.Scanner;
+
+public class roughWork {
+//    Scanner sc = new Scanner(System.in);
+//    public class a{
+//        int b=0;
+//    }
+//    public static void main(String[] args) {
+//        System.out.println("1"+ new Integer(3))+2-1);
+//        System.out.println(Integer.valueOf(3)+2+"1");
+//    }
+}
