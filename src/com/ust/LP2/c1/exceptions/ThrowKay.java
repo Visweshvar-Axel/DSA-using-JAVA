e788f87330a8d9e9bfd14a4f88d9f320766b4653
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-26 22:48:51 +0530

    ThrowKay i

--- src/com/ust/LP2/c1/exceptions/ThrowKay.java
+++ src/com/ust/LP2/c1/exceptions/ThrowKay.java
@@ -1,5 +1,12 @@
 package com.ust.LP2.c1.exceptions;
 
 public class ThrowKay {
+    static void validate(int age) {
+        if (age < 18) throw new ArithmeticException("Age is not enough");
+        else System.out.println("Welcome to vote");
+    }
 
+    public static void main(String[] args) {
+        validate(15);
+    }
 }
