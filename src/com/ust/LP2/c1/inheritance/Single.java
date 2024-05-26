1189338520020c7c0fd87f5bfb2a51f05b94fc6f
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-26 14:00:39 +0530

    Single imp

--- src/com/ust/LP2/c1/inheritance/Single.java
+++ src/com/ust/LP2/c1/inheritance/Single.java
@@ -1,4 +1,32 @@
 package com.ust.LP2.c1.inheritance;
 
 public class Single {
+    public static class Animal{
+        protected String color;
+        public Animal(String color){
+            this.color = color;
+        }
+        public void makeSound(){
+            System.out.println("******");
+        }
+    }
+    public static class Cat extends Animal{
+        String name;
+        public Cat(String name,String color){
+            super(color);
+            this.name = name;
+        }
+        @Override
+        public void makeSound(){
+            System.out.println("meow.....");
+        }
+    }
+
+    public static void main(String[] args) {
+        Animal animal = new Animal("red");
+        Cat cat = new Cat("atiya","white");
+        System.out.println("Anumal color:"+animal.color);
+        System.out.println("cat name:"+cat.name);
+        System.out.println("cat color:"+cat.color);
+    }
 }
