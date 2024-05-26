3dda68430254c54be44127ef2f933cff625bd9a4
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-26 19:55:23 +0530

    Lexo debugged

--- src/livesession/lexo.java
+++ src/livesession/lexo.java
@@ -6,14 +6,14 @@ public class lexo {
         String str1 = "listen";
         String str2 = "silent";
         for(int i=0;i<str1.length();i++){
-            alp[(int) str1.charAt(i)-95-1]++;
+            alp[(int) str1.charAt(i)-96-1]++;
         }
         for(int i=0;i<str2.length();i++){
-            alp[(int) str2.charAt(i)-95-1]--;
+            alp[(int) str2.charAt(i)-96-1]--;
         }
         int sum=0;
-        for(int i=0;i<24;i++){
-            sum+=alp[i];
+        for(int i=0;i<26;i++){
+            if(alp[i] > 0) sum+=alp[i];
         }
         if(sum==0) System.out.println("yes");
         else System.out.println("not");
