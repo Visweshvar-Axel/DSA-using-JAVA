92182f629c735769ebb6b9371f6b23c51a559766
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-26 22:25:10 +0530

    NestedTry i

--- src/com/ust/LP2/c1/exceptions/NestedTry.java
+++ src/com/ust/LP2/c1/exceptions/NestedTry.java
@@ -2,6 +2,20 @@ package com.ust.LP2.c1.exceptions;
 
 public class NestedTry {
     public static void main(String[] args) {
-
+        try {
+            try {
+                int[] a = new int[5];
+                a[5] = 0;
+            } catch (ArrayIndexOutOfBoundsException e){
+                System.out.println(e);
+            }
+            try {
+                System.out.println(10/0);
+            } catch (ArithmeticException e){
+                System.out.println(e);
+            }
+        } catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
