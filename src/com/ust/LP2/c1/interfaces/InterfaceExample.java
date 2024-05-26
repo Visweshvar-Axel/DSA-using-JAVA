5cd102285c223995fccf6dedbd25818c468a3f37
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-26 23:46:36 +0530

    InterfaceExample i

--- src/com/ust/LP2/c1/interfaces/InterfaceExample.java
+++ src/com/ust/LP2/c1/interfaces/InterfaceExample.java
@@ -1,5 +1,13 @@
 package com.ust.LP2.c1.interfaces;
-
+interface Animal{
+    void sound();
+}
 public class InterfaceExample implements Animal {
-
+    public void sound(){
+        System.out.println("Interface method implemented");
+    }
+    public static void main(String[] args) {
+        Animal cat = new InterfaceExample();
+        cat.sound();
+    }
 }
