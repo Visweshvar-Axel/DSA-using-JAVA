b62dc6cf80675a451650569175914fa6abd88dae
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-26 22:15:00 +0530

    TryCatch i

--- src/com/ust/LP2/c1/exceptions/TryCatch.java
+++ src/com/ust/LP2/c1/exceptions/TryCatch.java
@@ -2,6 +2,10 @@ package com.ust.LP2.c1.exceptions;
 
 public class TryCatch {
     public static void main(String[] args) {
-
+        try {
+            System.out.println(10/0);
+        } catch (ArithmeticException e){
+            System.out.println(e);
+        }
     }
 }
