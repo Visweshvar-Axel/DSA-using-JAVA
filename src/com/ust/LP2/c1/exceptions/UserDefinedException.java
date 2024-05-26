8e2600bc0a73a15cf539ed76039fd5197115848e
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-26 23:11:12 +0530

    UserDefinedException i

--- src/com/ust/LP2/c1/exceptions/UserDefinedException.java
+++ src/com/ust/LP2/c1/exceptions/UserDefinedException.java
@@ -1,5 +1,20 @@
 package com.ust.LP2.c1.exceptions;
+class AgeBelowException extends Exception{
 
+    private static final long serialVersionUID = 1246524410975209203L;
+
+    AgeBelowException(String s){super(s);}
+}
 public class UserDefinedException {
-
+    static void validate(int age) throws AgeBelowException {
+        if (age < 18) throw new AgeBelowException("Age is not enough");
+        else System.out.println("Welcome to vote");
+    }
+    public static void main(String[] args) {
+        try {
+            validate(15);
+        } catch (AgeBelowException e) {
+            System.out.println(e);
+        }
+    }
 }
