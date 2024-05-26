de0ebf011857277c3a796a521be7e42c8d0a328e
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-26 13:27:28 +0530

    car i

--- src/com/ust/LP2/c1/constructor/car1.java
+++ src/com/ust/LP2/c1/constructor/car1.java
@@ -2,4 +2,10 @@ package com.ust.LP2.c1.constructor;
 
 public class car1 {
     String name;
+
+    public static void main(String[] args) {
+        car1 car1 = new car1();
+        car1.name = "BMW";
+        System.out.println("car1: "+car1.name);
+    }
 }
