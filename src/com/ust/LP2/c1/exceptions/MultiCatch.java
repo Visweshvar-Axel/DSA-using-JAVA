d5103b25abc135bcb690c5c2c9999ab49ed11c40
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-26 22:21:46 +0530

    MultiCatch i

--- src/com/ust/LP2/c1/exceptions/MultiCatch.java
+++ src/com/ust/LP2/c1/exceptions/MultiCatch.java
@@ -2,6 +2,14 @@ package com.ust.LP2.c1.exceptions;
 
 public class MultiCatch {
     public static void main(String[] args) {
-
+        try {
+            int[] a = new int[5];
+            a[5] = 0;
+            System.out.println(10/0);
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println(e);
+        } catch (ArithmeticException e){
+            System.out.println(e);
+        }
     }
 }
