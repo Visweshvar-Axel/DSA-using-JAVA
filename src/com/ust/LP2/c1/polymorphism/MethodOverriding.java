d85c50d45a231989e64a0a4bf756eb322bd48d16
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-26 19:14:27 +0530

    MethodOverriding i

--- src/com/ust/LP2/c1/polymorphism/MethodOverriding.java
+++ src/com/ust/LP2/c1/polymorphism/MethodOverriding.java
@@ -1,5 +1,32 @@
 package com.ust.LP2.c1.polymorphism;
 
 public class MethodOverriding {
-    
+    public static class Vehicle{
+        String color;
+        protected Vehicle(String color){this.color = color;}
+        void run(){
+            System.out.println("Running...!");
+        }
+    }
+    public static class Bike extends Vehicle{
+        private String name;
+        public Bike(String name, String color){
+            super(color);
+            this.name = name;
+            //this.color = color;
+        }
+        @Override
+        void run(){
+            System.out.println("my "+this.name+" is Running...!");
+        }
+    }
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle("red");
+        Bike bike = new Bike("BMW","Black");
+        System.out.println("vehicle color:"+vehicle.color);
+        System.out.println("bike name:"+bike.name);
+        vehicle.run();
+        System.out.println("bike color:"+bike.color);
+        bike.run();
+    }
 }
