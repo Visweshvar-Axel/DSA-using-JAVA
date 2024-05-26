f855da638a9e46fcbce6dbca9470115905381c4e
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-26 22:28:10 +0530

    Finally i

--- src/com/ust/LP2/c1/exceptions/Finally.java
+++ src/com/ust/LP2/c1/exceptions/Finally.java
@@ -2,6 +2,14 @@ package com.ust.LP2.c1.exceptions;
 
 public class Finally {
     public static void main(String[] args) {
-
+        try {
+            System.out.println(10/0);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println(e);
+        }
+        finally {
+            System.out.println("Finally will always executed");
+        }
     }
 }
