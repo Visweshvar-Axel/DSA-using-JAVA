3976074eb30ffa2b34ef21bea3f8819b4d136141
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-26 19:25:55 +0530

    Abstraction i

--- src/com/ust/LP2/c1/abstraction/Calculatorabstract.java
+++ src/com/ust/LP2/c1/abstraction/Calculatorabstract.java
@@ -12,5 +12,9 @@ class MyCal extends Calculator{
     }
 }
 public class Calculatorabstract {
-
+    public static void main(String[] args) {
+        MyCal myCal = new MyCal();
+        myCal.add();
+        myCal.mul();
+    }
 }
