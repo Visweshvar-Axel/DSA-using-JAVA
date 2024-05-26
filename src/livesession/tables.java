5e112765556d869e250fc12bf5645b4dec67842d
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-26 11:51:07 +0530

    tables dangling comment-

--- src/livesession/tables.java
+++ src/livesession/tables.java
@@ -11,16 +11,16 @@ public class tables {
         for(int i = 1;i <=10;i++) System.out.println(t+" x "+i+" = "+(t*i));
     }
 }
-/**
- * Enter a table number: 10
- * 10 x 1 = 10
- * 10 x 2 = 20
- * 10 x 3 = 30
- * 10 x 4 = 40
- * 10 x 5 = 50
- * 10 x 6 = 60
- * 10 x 7 = 70
- * 10 x 8 = 80
- * 10 x 9 = 90
- * 10 x 10 = 100
- * */
+/*
+  Enter a table number: 10
+  10 x 1 = 10
+  10 x 2 = 20
+  10 x 3 = 30
+  10 x 4 = 40
+  10 x 5 = 50
+  10 x 6 = 60
+  10 x 7 = 70
+  10 x 8 = 80
+  10 x 9 = 90
+  10 x 10 = 100
+  */
