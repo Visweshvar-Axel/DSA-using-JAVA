151ac53147ece9431b13ad847df7f0392d510bbc
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-26 16:45:00 +0530

    WithoutMod i

--- src/com/ust/LP1/c2/assignments/WithoutMod.java
+++ src/com/ust/LP1/c2/assignments/WithoutMod.java
@@ -2,6 +2,7 @@ package com.ust.LP1.c2.assignments;
 
 public class WithoutMod {
     public static void main(String[] args) {
-
+        int a = 11,b = 4;
+        System.out.println(a-(b*(a/b)));
     }
 }
