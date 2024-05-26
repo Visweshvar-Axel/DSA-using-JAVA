9aad5aa8a671af2acf5c1e641d73dcf80a9bf3de
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-26 22:53:41 +0530

    ThrowsKey i

--- src/com/ust/LP2/c1/exceptions/ThrowsKey.java
+++ src/com/ust/LP2/c1/exceptions/ThrowsKey.java
@@ -1,5 +1,21 @@
 package com.ust.LP2.c1.exceptions;
 
 public class ThrowsKey {
-
+    void m() throws ArithmeticException {
+        throw new ArithmeticException("Throws keyword");
+    }
+    void n() throws ArithmeticException {
+        m();
+    }
+    void o(){
+        try {
+            m();
+        } catch (ArithmeticException e){
+            System.out.println(e);
+        }
+    }
+    public static void main(String[] args) {
+        ThrowsKey throwsKey = new ThrowsKey();
+        throwsKey.o();
+    }
 }
