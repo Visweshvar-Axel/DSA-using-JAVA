7a8a90243f6b98b1e64e38bff082f7860968554e
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-26 11:55:45 +0530

    Dog color +

--- src/com/ust/LP2/c1/Dog.java
+++ src/com/ust/LP2/c1/Dog.java
@@ -2,9 +2,11 @@ package com.ust.LP2.c1;
 
 public class Dog {
     String name;
+    String color;
 
     public static void main(String[] args) {
         Dog dog = new Dog();  //craeting object
         dog.name = "rick";
+        dog.color = "black";
     }
 }
