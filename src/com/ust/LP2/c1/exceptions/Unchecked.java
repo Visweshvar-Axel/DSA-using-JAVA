9d17c719640c7ff8f85cf9c724cb940f634bb461
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-26 19:49:16 +0530

    Unchecked i

--- src/com/ust/LP2/c1/exceptions/Unchecked.java
+++ src/com/ust/LP2/c1/exceptions/Unchecked.java
@@ -1,5 +1,7 @@
 package com.ust.LP2.c1.exceptions;
 
 public class Unchecked {
-
+    public static void main(String[] args) {
+        System.out.println(15/0); //java.lang.ArithmeticException: / by zero
+    }
 }
