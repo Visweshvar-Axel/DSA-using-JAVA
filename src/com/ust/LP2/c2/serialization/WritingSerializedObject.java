d1768762409b6b2b2b985e32955790360ed9ba17
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-28 19:55:56 +0530

    WritingSerializedObject happening

--- src/com/ust/LP2/c2/serialization/WritingSerializedObject.java
+++ src/com/ust/LP2/c2/serialization/WritingSerializedObject.java
@@ -25,6 +25,29 @@ public class WritingSerializedObject {
         //4 close the ObjectOutputStream
         objstr.close();
     }
+    /* *
+    * 1) object on the heap:
+    * object has state: represented by value of instance variables
+    * these values make one instance of a class different from another instances of the same class
+    *
+    * Class foo{
+    * int height;
+    * int weignt
+    * }
+    * foo f = new foo();
+    * f.setHeight(50);
+    * f.setWeight(50);
+    *
+    * 2) objects serialized:
+    * serialized objects save the values of the instance variables, so that an identical instance can be brought back to life on the heap:
+    * FileOutputStream file = new FileOutputStream("foo.ser");
+    * ObjectOutputStream objstr = new ObjectOutputStream(file);
+    * objstr.writeObject(f);
+    *
+    * the instance variable values for width and height are saved to the file "f. ser"
+    * along with a little more information the JVM needs to restore the object(like what's its class type)
+    * */
+
     // the good oop design says:
     //each class does one thing well!! I
 
