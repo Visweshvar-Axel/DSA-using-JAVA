75fa50c95892d550a737c1c2e3ce0ddec1b7224e
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-28 19:12:49 +0530

    WritingSerializedObject imp

--- src/com/ust/LP2/c2/serialization/WritingSerializedObject.java
+++ src/com/ust/LP2/c2/serialization/WritingSerializedObject.java
@@ -1,5 +1,20 @@
 package com.ust.LP2.c2.serialization;
 
-public class WritingSerializedObject {
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
+public class WritingSerializedObject {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        FileOutputStream file = new FileOutputStream("GameCharacter.ser");
+
+        ObjectOutputStream objstr = new ObjectOutputStream(file);
+
+        objstr.writeObject(characterOne);
+        objstr.writeObject(characterTwo);
+        objstr.writeObject(characterThree);
+
+        objstr.close();
+    }
 }
