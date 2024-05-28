6a5d630ef68bde6ec6fb3556accea5a259ccce14
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-29 02:08:35 +0530

    box

--- src/com/ust/LP2/c2/serialization/Box.java
+++ src/com/ust/LP2/c2/serialization/Box.java
@@ -3,29 +3,22 @@ package com.ust.LP2.c2.serialization;
 import java.io.*;
 
 public class Box implements Serializable {
-
     private int width;
     private int height;
-
     public Box(){}
-
     public Box(int width, int height) {
         this.width = width;
         this.height = height;
     }
-
     public int getWidth() {
         return width;
     }
-
     public int getHeight() {
         return height;
     }
-
     public void setWidth(int width) {
         this.width = width;
     }
-
     public void setHeight(int height) {
         this.height = height;
     }
