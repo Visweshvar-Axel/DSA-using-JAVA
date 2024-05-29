a89aa2e18a604001e987722e4ea7749a2788a9c5
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-29 22:58:39 +0530

    duck

--- src/com/ust/LP2/c2/serialization/Duck.java
+++ src/com/ust/LP2/c2/serialization/Duck.java
@@ -2,7 +2,8 @@ package com.ust.LP2.c2.serialization;
 
 import java.io.Serializable;
 
-public class Duck implements Serializable {
+//public class Duck implements Serializable {
+public class Duck {
     // it doesn't implement serializable,
     // so when we try to serialize a pond object, it fails because the pond's
     // duck instance variable can't be saved
