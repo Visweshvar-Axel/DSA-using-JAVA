4cfa8789d8f50b8bae1ab54d35fb283bea15660a
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-29 22:46:48 +0530

    Pond imp

--- src/com/ust/LP2/c2/serialization/Pond.java
+++ src/com/ust/LP2/c2/serialization/Pond.java
@@ -1,8 +1,24 @@
 package com.ust.LP2.c2.serialization;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class Pond implements Serializable {
+    //pond object can be serialized
     private Duck duck = new Duck();
 
+    public static void main(String[] args) throws IOException {
+        Pond pond = new Pond();
+        ObjectOutputStream objStr = null;
+        try {
+            FileOutputStream file = new FileOutputStream("pond.ser");
+            objStr = new ObjectOutputStream(file);
+            objStr.writeObject(pond);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        objStr.close();
+    }
 }
