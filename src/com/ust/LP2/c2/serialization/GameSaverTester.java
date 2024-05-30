78617403bf947d8451e6a5f5f83563f66f1bb3a5
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-31 00:23:31 +0530

    GameSaverTester imp

--- src/com/ust/LP2/c2/serialization/GameSaverTester.java
+++ src/com/ust/LP2/c2/serialization/GameSaverTester.java
@@ -1,4 +1,20 @@
 package com.ust.LP2.c2.serialization;
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
 public class GameSaverTester {
+    public static void main(String[] args) {
+        // create three objects of GameCharacter class
+        GameCharacter one = new GameCharacter(50,"Viswa",new String[]{"bare", "bomb"});
+        GameCharacter two = new GameCharacter(200,"axel",new String[]{"bow", "arrow","bomb"});
+        GameCharacter three = new GameCharacter(120,"anu",new String[]{"knife", "rope"});
+        //serilazing the objects
+        try {
+            FileOutputStream file = new FileOutputStream("Game.ser");
+            ObjectOutputStream objSer = new ObjectOutputStream(file);
+
+        }catch (Exception e){e.printStackTrace();}
+
+    }
 }
