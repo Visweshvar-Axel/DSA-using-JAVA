e28bdc55416706995b0a0b0fcda7f2fad423ff72
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-31 00:17:41 +0530

    GameCharacter imp

--- src/com/ust/LP2/c2/serialization/GameCharacter.java
+++ src/com/ust/LP2/c2/serialization/GameCharacter.java
@@ -1,5 +1,32 @@
 package com.ust.LP2.c2.serialization;
 
-public class GameCharacter {
+import java.io.Serializable;
 
+public class GameCharacter implements Serializable {
+
+    private int power;
+    private String type;
+    private String[] weapons;
+
+    public GameCharacter(){}
+
+    public GameCharacter(int power, String type, String[] weapons) {
+        this.power = power;
+        this.type = type;
+        this.weapons = weapons;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getWeapons() {
+        String weaponList = "";
+        for(String s : weapons) weaponList+=s;
+        return weaponList;
+    }
 }
