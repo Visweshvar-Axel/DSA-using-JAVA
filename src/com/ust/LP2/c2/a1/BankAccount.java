0cbb05b16bb99c2000f7570e91a5b12340f8c568
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-31 13:02:29 +0530

    BankAccount imp

--- src/com/ust/LP2/c2/a1/BankAccount.java
+++ src/com/ust/LP2/c2/a1/BankAccount.java
@@ -1,5 +1,47 @@
 package com.ust.LP2.c2.a1;
 
 public class BankAccount {
+    private int accNo;
+    private String name;
+    private Double balance;
+    private String password;
 
+    public BankAccount(int accNo, String name, Double balance, String password) {
+        this.accNo = accNo;
+        this.name = name;
+        this.balance = balance;
+        this.password = password;
+    }
+
+    public int getAccNo() {
+        return accNo;
+    }
+
+    public void setAccNo(int accNo) {
+        this.accNo = accNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
