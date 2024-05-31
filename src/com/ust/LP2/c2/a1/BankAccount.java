bfb85dbc7612599b4f473ee6caf6c687a65fd0d1
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-31 22:21:18 +0530

    BankAccountTesting +

--- src/com/ust/LP2/c2/a1/BankAccount.java
+++ src/com/ust/LP2/c2/a1/BankAccount.java
@@ -3,25 +3,25 @@ package com.ust.LP2.c2.a1;
 import java.io.Serializable;
 
 public class BankAccount implements Serializable {
-    private int accNo;
+    private String accNo;
     private String name;
     private Double balance;
     private String password;
 
     public BankAccount() {}
 
-    public BankAccount(int accNo, String name, Double balance, String password) {
+    public BankAccount(String accNo, String name, Double balance, String password) {
         this.accNo = accNo;
         this.name = name;
         this.balance = balance;
         this.password = password;
     }
 
-    public int getAccNo() {
+    public String getAccNo() {
         return accNo;
     }
 
-    public void setAccNo(int accNo) {
+    public void setAccNo(String accNo) {
         this.accNo = accNo;
     }
 
