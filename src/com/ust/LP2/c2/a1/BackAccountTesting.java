208f651628c02909db8f816d38a0275ed8221b74
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-05-31 22:21:21 +0530

    BankAccountTesting +

--- src/com/ust/LP2/c2/a1/BackAccountTesting.java
+++ src/com/ust/LP2/c2/a1/BackAccountTesting.java
@@ -1,5 +1,5 @@
 package com.ust.LP2.c2.a1;
 
 public class BackAccountTesting {
-    BankAccount acc1 = new BankAccount(1234567891234,"Viswa","")
+    BankAccount acc1 = new BankAccount("1234567891234","Viswa",2000.0,"vISWA123");
 }
