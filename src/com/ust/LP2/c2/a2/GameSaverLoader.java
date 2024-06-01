637a5c0a6f8734d4e70d67f5f29fe6bfa63c8e06
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-01 13:43:05 +0530

    GameSaverLoader g

--- src/com/ust/LP2/c2/a2/GameSaverLoader.java
+++ src/com/ust/LP2/c2/a2/GameSaverLoader.java
@@ -1,7 +1,31 @@
 package com.ust.LP2.c2.a2;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class GameSaverLoader {
     public static void gameSaveM(GameCharacter[] objs){
-
+        try {
+            FileOutputStream file = new FileOutputStream("GameData.ser");
+            ObjectOutputStream ob = new ObjectOutputStream(file);
+            for (GameCharacter ch: objs) ob.writeObject(ch);
+            ob.close();
+        } catch (Exception e) {e.printStackTrace();}
+    }
+    public static GameCharacter[] gatGameChar(int num){
+        GameCharacter[] objs = new GameCharacter[num];
+        try {
+            FileInputStream file = new  FileInputStream("GameData.ser");
+            ObjectInputStream ob = new ObjectInputStream(file);
+            for (int i = 0;i < num;i++) objs[i] = (GameCharacter) ob.readObject();
+            ob.close();
+        } catch (Exception e) {e.printStackTrace();}
+        return objs;
+    }
+    public static void main(String[] args) {
+        GameCharacter Gamechar = new GameCharacter()
+        GameCharacter newGamechar =
     }
 }
