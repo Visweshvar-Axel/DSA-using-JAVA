3497e244f63f068784372995484f704a7ea72a1b
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-01 14:21:12 +0530

    Data.java imp

--- src/com/ust/LP2/c2/a3/Data.java
+++ src/com/ust/LP2/c2/a3/Data.java
@@ -1,4 +1,30 @@
 package com.ust.LP2.c2.a3;
 
 public class Data {
+    private int id;
+    private String msg;
+    public Data(){}
+    public Data(int id, String msg) {
+        this.id = id;
+        this.msg = msg;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getMsg() {
+        return msg;
+    }
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+    @Override
+    public String toString() {
+        return "Data{" +
+                "id=" + id +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
 }
