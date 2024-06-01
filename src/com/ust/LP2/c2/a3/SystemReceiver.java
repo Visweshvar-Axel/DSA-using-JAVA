f02377ac3caf6f7fcd6d290acbc2045ad2d5571f
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-01 14:39:23 +0530

    SystemReceiver imp

--- src/com/ust/LP2/c2/a3/SystemReceiver.java
+++ src/com/ust/LP2/c2/a3/SystemReceiver.java
@@ -1,14 +1,18 @@
 package com.ust.LP2.c2.a3;
 
-import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SystemReceiver {
     public static void main(String[] args) {
-        Data data = new Data(12,"Hello Viswa");
-        try (Socket socket = new Socket("localhost",12345)){
-            ObjectOutputStream ob = new ObjectOutputStream(socket.getOutputStream());
-            ob.writeObject(data);
+        try (ServerSocket serverSocket = new ServerSocket(12345)) {
+            while (true){
+                try (Socket socket = serverSocket.accept()) {
+                    ObjectInputStream ob = new ObjectInputStream(socket.getInputStream());
+                    Data data = (Data) ob.readObject();
+                } catch (Exception e) {e.printStackTrace();}
+            }
         } catch (Exception e) {e.printStackTrace();}
     }
 }
