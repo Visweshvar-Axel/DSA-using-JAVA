507692b1bfccdcc093b199c4e6afff00a7be74d9
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-01 14:30:42 +0530

    SystemSender ServerSocket

--- src/com/ust/LP2/c2/a3/SystemSender.java
+++ src/com/ust/LP2/c2/a3/SystemSender.java
@@ -1,4 +1,11 @@
 package com.ust.LP2.c2.a3;
 
+import java.net.ServerSocket;
+
 public class SystemSender {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(12345)) {
+
+        } catch (Exception e) {e.printStackTrace();}
+    }
 }
