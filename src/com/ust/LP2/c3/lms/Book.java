e31f19e31c22be7ae2333a1bb0aca209b75e80a5
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-02 23:43:03 +0530

    Book.java

--- src/com/ust/LP2/c3/lms/Book.java
+++ src/com/ust/LP2/c3/lms/Book.java
@@ -43,4 +43,8 @@ public class Book {
     public void setTitle(String title) {
         this.title = title;
     }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
