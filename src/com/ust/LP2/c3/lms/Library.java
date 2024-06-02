4476e9609a91e4c3d2fc08ec4a81b71186ed0224
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-02 23:52:39 +0530

    Library

--- src/com/ust/LP2/c3/lms/Library.java
+++ src/com/ust/LP2/c3/lms/Library.java
@@ -28,7 +28,6 @@ public class Library {
     public void setlName(String lName) {
         this.lName = lName;
     }
-
     public void setAddress(String address) {
         this.address = address;
     }
