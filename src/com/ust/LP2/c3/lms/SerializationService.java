7e7060144a0ca8a6e2a8518ef529c465cbb494ee
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-03 23:14:56 +0530

    SerializationService

--- src/com/ust/LP2/c3/lms/SerializationService.java
+++ src/com/ust/LP2/c3/lms/SerializationService.java
@@ -1,4 +1,6 @@
 package com.ust.LP2.c3.lms;
 
 public interface SerializationService {
+    public void serializeLibrary(Library library,String fileName);
+    public Library deserializeLibrary(String fileName);
 }
