78b0d4424fcd1836bb50f980f2bbfcce9e9e3c33
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-03 23:58:40 +0530

    MainUtil

--- src/com/ust/LP2/c3/lms/MainUtil.java
+++ src/com/ust/LP2/c3/lms/MainUtil.java
@@ -2,6 +2,10 @@ package com.ust.LP2.c3.lms;
 
 public class MainUtil {
     public static void main(String[] args) {
-
+        Book obj1 = new Book("A-z1","Java","Viswa",100,5,"program");
+        Book obj2 = new Book("asd","python","Axel",50,3,"program");
+        Library lb = new Library();
+        lb.setlName("ViswaLib");
+        lb.setAddress("Salem");
     }
 }
