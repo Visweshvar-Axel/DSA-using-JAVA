759c6a2950cc6630807d03f5c1e7f7136c8efc70
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-03 23:33:25 +0530

    SerializationHandler

--- src/com/ust/LP2/c3/lms/SerializationHandler.java
+++ src/com/ust/LP2/c3/lms/SerializationHandler.java
@@ -1,7 +1,14 @@
 package com.ust.LP2.c3.lms;
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
 public class SerializationHandler implements SerializationService {
     public void serializeLibrary (Library library, String fileName) {
-
+        try {
+            FileOutputStream fout = new FileOutputStream(fileName);
+            ObjectOutputStream obj = new ObjectOutputStream(fout);
+            obj.writeObject(library);
+        } catch (Exception e) { e.printStackTrace(); }
     }
 }
