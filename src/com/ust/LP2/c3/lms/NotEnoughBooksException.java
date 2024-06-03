f4bdaec0a28d971c44aeebeb5d07a0f1864afa3e
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-03 23:52:15 +0530

    NotEnoughBooksException

--- src/com/ust/LP2/c3/lms/NotEnoughBooksException.java
+++ src/com/ust/LP2/c3/lms/NotEnoughBooksException.java
@@ -1,6 +1,8 @@
 package com.ust.LP2.c3.lms;
 
 public class NotEnoughBooksException extends Exception{
+    private static final long serialVersionUID = 1660495612304903136L;
+
     public NotEnoughBooksException(String message) {
         super(message);
     }
