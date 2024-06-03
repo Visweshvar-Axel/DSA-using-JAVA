74b40739329536c612a40ae0dd449b3b690de98a
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-03 23:52:01 +0530

    BookNotFoundException

--- src/com/ust/LP2/c3/lms/BookNotFoundException.java
+++ src/com/ust/LP2/c3/lms/BookNotFoundException.java
@@ -1,6 +1,8 @@
 package com.ust.LP2.c3.lms;
 
 public class BookNotFoundException extends Exception {
+    private static final long serialVersionUID = -2934943317481636132L;
+
     public BookNotFoundException(String message) {
         super(message);
     }
