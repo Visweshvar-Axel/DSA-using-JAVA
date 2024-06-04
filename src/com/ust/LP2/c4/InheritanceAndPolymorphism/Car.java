6eedd3c1864dc446c312b5560dc3c48e108456c7
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-05 00:32:03 +0530

    Car

--- src/com/ust/LP2/c4/InheritanceAndPolymorphism/Car.java
+++ src/com/ust/LP2/c4/InheritanceAndPolymorphism/Car.java
@@ -3,7 +3,6 @@ package com.ust.LP2.c4.InheritanceAndPolymorphism;
 public class Car extends Vehicle {
     public Car() {
     }
-
     public Car(String make, String model) {
         super(make, model);
     }
