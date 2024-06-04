2d75d1e311ff0563215617dc41dd7410c217c1d0
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-05 00:35:57 +0530

    Motorcycle imp

--- src/com/ust/LP2/c4/InheritanceAndPolymorphism/Motorcycle.java
+++ src/com/ust/LP2/c4/InheritanceAndPolymorphism/Motorcycle.java
@@ -6,7 +6,6 @@ public class Motorcycle extends Vehicle {
     public Motorcycle(String make, String model) {
         super(make, model);
     }
-
     @Override
     public void startEngine() {
         System.out.println("troom... troom...");
