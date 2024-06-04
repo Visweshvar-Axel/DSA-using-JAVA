b66cb971237f2733255922ba4e6195ef6cb0b9dd
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-05 00:34:23 +0530

    Motorcycle

--- src/com/ust/LP2/c4/InheritanceAndPolymorphism/Motorcycle.java
+++ src/com/ust/LP2/c4/InheritanceAndPolymorphism/Motorcycle.java
@@ -3,4 +3,8 @@ package com.ust.LP2.c4.InheritanceAndPolymorphism;
 public class Motorcycle extends Vehicle {
     public Motorcycle() {
     }
+
+    public Motorcycle(String make, String model) {
+        super(make, model);
+    }
 }
