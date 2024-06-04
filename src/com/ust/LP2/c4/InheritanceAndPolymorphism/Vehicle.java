ee6e49f8649c75392eef2fca4753c9d04eaa6ce5
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-05 00:28:30 +0530

    Vehicle

--- src/com/ust/LP2/c4/InheritanceAndPolymorphism/Vehicle.java
+++ src/com/ust/LP2/c4/InheritanceAndPolymorphism/Vehicle.java
@@ -3,6 +3,10 @@ package com.ust.LP2.c4.InheritanceAndPolymorphism;
 public class Vehicle {
     private String make;
     private String model;
+
+    public Vehicle() {
+    }
+
     public void startEngine() {
         System.out.println("worrr... worrr...");
     }
