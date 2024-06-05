89c14b6e85f4e2a3441ae7b9f97c7f8eb987d223
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-06 00:08:55 +0530

    polymorphismMain

--- src/com/ust/LP2/c4/InheritanceAndPolymorphism/polymorphismMain.java
+++ src/com/ust/LP2/c4/InheritanceAndPolymorphism/polymorphismMain.java
@@ -2,7 +2,14 @@ package com.ust.LP2.c4.InheritanceAndPolymorphism;
 
 public class polymorphismMain {
     public static void main(String[] args) {
-        Vehicle toyota = new Car("toyota","1332")
-        Car ford = new Car("ford","1332")
+        Vehicle toyota = new Car("toyota","1332");
+        Car ford = new Car("ford","1333");
+        Motorcycle honda = new Motorcycle("honda","1333");
+        System.out.println(toyota.toString());
+        System.out.println(toyota.startEngine(););
+        System.out.println(ford.toString());
+        System.out.println(ford.startEngine(););
+        System.out.println(honda.toString());
+        System.out.println(honda.startEngine(););
     }
 }
