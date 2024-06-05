ca4cbfe2ef1c267d6ffc9c942ab5193c118ec5d1
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-05 19:56:23 +0530

    pet.java

---
+++ src/livesession/oops/pet.java
@@ -0,0 +1,24 @@
+package livesession.oops;
+
+public class pet {
+    private String name;
+    private String breed;
+    private int age;
+    private int legs;
+
+    public pet(String name, String breed, int age, int legs) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.legs = legs;
+    }
+    @Override
+    public String toString() {
+        return "pet{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                ", age=" + age +
+                ", legs=" + legs +
+                '}';
+    }
+}
