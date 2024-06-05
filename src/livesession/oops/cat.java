8bf96a72b06fed5a39bd38edb92b6fbdb7d0babb
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-05 19:55:25 +0530

    cat.java

--- src/livesession/oops/cat.java
+++ src/livesession/oops/cat.java
@@ -1,5 +1,10 @@
 package livesession.oops;
 
 public class cat extends pet{
+    private String color;
 
+    public cat(String name, String breed, int age, int legs, String color) {
+        super(name, breed, age, legs);
+        this.color = color;
+    }
 }
