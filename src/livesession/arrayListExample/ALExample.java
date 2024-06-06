0ee3bba05eaaadddf3897f4854550eef0b996fa2
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-07 01:51:44 +0530

    ALExample

--- src/livesession/arrayListExample/ALExample.java
+++ src/livesession/arrayListExample/ALExample.java
@@ -1,7 +1,13 @@
 package livesession.arrayListExample;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ALExample {
     public static void main(String[] args) {
-
+        List<String> al= new ArrayList<>();
+        al.add("Viswa");
+        al.add("Visweshvar");
+        al.add("Axel");
     }
 }
