9630744544f40a914cddeaacf0184a560fe63593
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-07 18:16:44 +0530

    ALExample

--- src/livesession/arrayListExample/ALExample.java
+++ src/livesession/arrayListExample/ALExample.java
@@ -1,14 +1,19 @@
 package livesession.arrayListExample;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class ALExample {
     public static void main(String[] args) {
         List<String> al= new ArrayList<>();
         al.add("Viswa");
-        al.add("Visweshvar");
+        al.add("Sarankumar");
         al.add("Axel");
-
+        al.add("janani");
+        al.add("jeswin");
+        for (Iterator<String> it = al.iterator(); it.hasNext(); ) {
+            System.out.print(it.next()+", ");
+        }
     }
 }
