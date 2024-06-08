e555dad0032d1ff0888339e9504f6d903c115868
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-08 17:19:24 +0530

    Bank

--- src/livesession/ExceptionHandling/Bank.java
+++ src/livesession/ExceptionHandling/Bank.java
@@ -31,7 +31,7 @@ public class Bank {
         System.out.println(b.checkBalance());
         b.deposit(400);
         System.out.println(b.checkBalance());
-        b.withdrawn(800);
+        b.withdrawn(800.0);
         System.out.println(b.checkBalance());
 
     }
