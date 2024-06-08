1ce33478875982d1858bf5c19b7d1a4eeb5ba79f
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-08 17:17:38 +0530

    Bank

--- src/livesession/ExceptionHandling/Bank.java
+++ src/livesession/ExceptionHandling/Bank.java
@@ -13,6 +13,8 @@ public class Bank {
     void withdrawn(double amount) throws MinimumBalance {
         if(balance < amount){
             throw new MinimumBalance();
+        } else {
+
         }
     }
 }
