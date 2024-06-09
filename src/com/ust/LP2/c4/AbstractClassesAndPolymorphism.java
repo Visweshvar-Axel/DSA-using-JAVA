fddc4d11c08b470d719febf32fc51b630f1ad586
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-09 23:25:31 +0530

    AbstractClassesAndPolymorphism

--- src/com/ust/LP2/c4/AbstractClassesAndPolymorphism.java
+++ src/com/ust/LP2/c4/AbstractClassesAndPolymorphism.java
@@ -1,4 +1,45 @@
 package com.ust.LP2.c4;
+abstract class Animal {
+    String name;
 
+    Animal(String name) {
+        this.name = name;
+    }
+
+    abstract void makeSound();
+}
+
+class Cat extends Animal {
+    Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println(name + " : meow");
+    }
+}
+
+class Dog extends Animal {
+    Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println(name + " : wool!");
+    }
+}
 public class AbstractClassesAndPolymorphism {
+    public static void main(String[] args) {
+        Animal[] animals = {
+                new Dog("atiya"),
+                new Cat("jeeni"),
+                new Cat("sham")
+        };
+
+        for (Animal a : animals) {
+            a.makeSound();
+        }
+    }
 }
