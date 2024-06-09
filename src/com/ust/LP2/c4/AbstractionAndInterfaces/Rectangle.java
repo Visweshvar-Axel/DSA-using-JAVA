c27e096f9319189cc489bcf4aaa146ae96f953ef
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-09 23:16:19 +0530

    Rectangle

--- src/com/ust/LP2/c4/AbstractionAndInterfaces/Rectangle.java
+++ src/com/ust/LP2/c4/AbstractionAndInterfaces/Rectangle.java
@@ -2,6 +2,12 @@ package com.ust.LP2.c4.AbstractionAndInterfaces;
 
 public class Rectangle implements Shape{
     private int length, breadth;
+
+    public Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
     @Override
     public double calculateArea() {
         return length*breadth;
