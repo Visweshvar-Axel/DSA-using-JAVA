0c32a0ab19a10fef252a9011924af515cf30e8d8
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-09 23:01:27 +0530

    BufferReaderExample

--- src/livesession/IOExample/BufferReaderExample.java
+++ src/livesession/IOExample/BufferReaderExample.java
@@ -1,10 +1,11 @@
 package livesession.IOExample;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BufferReaderExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         char c;
         String t;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
