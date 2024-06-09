b4d221da13119986fd25e97e8ab076770fe6bfe0
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-09 23:01:22 +0530

    BufferReaderExample

--- src/livesession/IOExample/BufferReaderExample.java
+++ src/livesession/IOExample/BufferReaderExample.java
@@ -8,5 +8,6 @@ public class BufferReaderExample {
         char c;
         String t;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        c = (char) br.read();
     }
 }
