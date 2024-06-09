fabef1b5e249bf23e745842f3e11c15206a09e94
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-09 23:13:09 +0530

    Circle

---
+++ src/com/ust/LP2/c4/AbstractionAndInterfaces/Circle.java
@@ -0,0 +1,14 @@
+package com.ust.LP2.c4.AbstractionAndInterfaces;
+
+public class Circle implements Shape{
+    private int radius;
+    @Override
+    public double calculateArea() {
+        return Math.PI*radius*radius;
+    }
+
+    @Override
+    public void displayShapeInfo() {
+
+    }
+}
