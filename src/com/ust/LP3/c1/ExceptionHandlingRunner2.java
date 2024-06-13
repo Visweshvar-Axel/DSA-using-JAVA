18e01d071606a1720d749bc5c7a39a62b46163dd
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-13 23:30:27 +0530

    ExceptionHandlingRunner2

--- src/com/ust/LP3/c1/ExceptionHandlingRunner2.java
+++ src/com/ust/LP3/c1/ExceptionHandlingRunner2.java
@@ -1,5 +1,17 @@
 package com.ust.LP3.c1;
 
 public class ExceptionHandlingRunner2 {
-
+    public static void main(String[] args) {
+        method1();
+    }
+    private static void method1() {
+        method2();
+    }
+    private static void method2() {
+        method3();
+    }
+    private static void method3() {
+        String s = null;
+        s.length();
+    }
 }
