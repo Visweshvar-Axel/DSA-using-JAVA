67bc98778ebc5ac78fc97696126a3ec20f1f7586
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-14 23:47:38 +0530

    ThrowingExceptionRunner

--- src/com/ust/LP3/c1/ThrowingExceptionRunner.java
+++ src/com/ust/LP3/c1/ThrowingExceptionRunner.java
@@ -1,6 +1,20 @@
 package com.ust.LP3.c1;
 class Amount {
+    private String currency;
+    private int amount;
 
+    public Amount(String currency, int amount) {
+        this.currency = currency;
+        this.amount = amount;
+    }
+    public void add
+    @Override
+    public String toString() {
+        return "Amount{" +
+                "currency='" + currency + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
 public class ThrowingExceptionRunner {
     public static void main(String[] args) {
