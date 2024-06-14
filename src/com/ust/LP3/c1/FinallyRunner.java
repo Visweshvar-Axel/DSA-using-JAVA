756107ff2722a69537ad623a050f9af58753e5c5
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-14 15:23:44 +0530

    FinallyRunner

--- src/com/ust/LP3/c1/FinallyRunner.java
+++ src/com/ust/LP3/c1/FinallyRunner.java
@@ -5,5 +5,9 @@ import java.util.Scanner;
 public class FinallyRunner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        try {
+            int[] arr = {1,2,3};
+            int num = arr[3];
+        } catch (Exception)
     }
 }
