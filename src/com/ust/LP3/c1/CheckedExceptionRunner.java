f3301db7afe424fe912224a24aab63da25a068d6
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-14 20:11:55 +0530

    CheckedExceptionRunner

--- src/com/ust/LP3/c1/CheckedExceptionRunner.java
+++ src/com/ust/LP3/c1/CheckedExceptionRunner.java
@@ -3,14 +3,20 @@ package com.ust.LP3.c1;
 public class CheckedExceptionRunner {
     public static void main(String[] args) {
         try {
-            someOtherMethod();
+            someOtherMethod1();
+            someOtherMethod2();
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    private static void someOtherMethod() throws InterruptedException {
+    private static void someOtherMethod2() throws RuntimeException {
+        String str = null;
+        str.length();
+    }
+
+    private static void someOtherMethod1() throws InterruptedException {
         Thread.sleep(2000);
     }
 }
