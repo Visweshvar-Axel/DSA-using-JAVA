71d1f9e292ddecd8fbfaf2cda800961df68c38ac
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-14 23:55:13 +0530

    ThrowingExceptionRunner

--- src/com/ust/LP3/c1/ThrowingExceptionRunner.java
+++ src/com/ust/LP3/c1/ThrowingExceptionRunner.java
@@ -22,6 +22,6 @@ public class ThrowingExceptionRunner {
         Amount amount1 = new Amount("USD",10);
         Amount amount2 = new Amount("ERO",20);
         amount1.add(amount2);
-        System.out.println();
+        System.out.println(amount1);
     }
 }
