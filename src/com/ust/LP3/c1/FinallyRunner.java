9a59b78264f96d8ac44fabe902a7790480a17536
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-14 15:22:08 +0530

    FinallyRunner

--- src/com/ust/LP3/c1/FinallyRunner.java
+++ src/com/ust/LP3/c1/FinallyRunner.java
@@ -1,7 +1,9 @@
 package com.ust.LP3.c1;
 
+import java.util.Scanner;
+
 public class FinallyRunner {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
     }
 }
