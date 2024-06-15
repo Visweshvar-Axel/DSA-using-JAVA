57017672e70f3e0e8a87aaf864827a7a99af36aa
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-15 22:26:07 +0530

    TryWithResourcesRunner

---
+++ src/com/ust/LP3/c1/TryWithResourcesRunner.java
@@ -0,0 +1,12 @@
+package com.ust.LP3.c1;
+
+import java.util.Scanner;
+
+public class TryWithResourcesRunner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+            int[] arr = {1,2,3};
+            int num = arr[3];
+
+    }
+}
