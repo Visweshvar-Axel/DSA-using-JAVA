32f8ab73f0343e4edd155905a952595a1b2416dd
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-15 22:20:55 +0530

    ThrowingExceptionRunner make it as unchecked

--- src/com/ust/LP3/c1/ThrowingExceptionRunner.java
+++ src/com/ust/LP3/c1/ThrowingExceptionRunner.java
@@ -23,7 +23,7 @@ class Amount {
     }
 }
 public class ThrowingExceptionRunner {
-    public static void main(String[] args) throws CurrencyDoNotMatchException {
+    public static void main(String[] args){
         Amount amount1 = new Amount("USD",10);
         Amount amount2 = new Amount("ERO",20);
         amount1.add(amount2);
