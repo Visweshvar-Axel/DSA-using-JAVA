7a3c991dafafea2204c8393d225e2779bbd2bc5b
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-15 22:27:43 +0530

    TryWithResourcesRunner

--- src/com/ust/LP3/c1/TryWithResourcesRunner.java
+++ src/com/ust/LP3/c1/TryWithResourcesRunner.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class TryWithResourcesRunner {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
+        try (Scanner scanner = new Scanner(System.in)) { // resource should be extended AutoClosable
             int[] arr = {1, 2, 3};
             int num = arr[3];
-        }
+        } // catch or finally is not mandatory
     }
 }
