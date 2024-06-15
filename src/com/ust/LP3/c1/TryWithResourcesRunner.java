76386ad0608c003d4efd731e34df9f252873868e
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-15 22:26:36 +0530

    TryWithResourcesRunner

--- src/com/ust/LP3/c1/TryWithResourcesRunner.java
+++ src/com/ust/LP3/c1/TryWithResourcesRunner.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class TryWithResourcesRunner {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-            int[] arr = {1,2,3};
+        try (Scanner scanner = new Scanner(System.in)) {
+            int[] arr = {1, 2, 3};
             int num = arr[3];
-
+        }
     }
 }
