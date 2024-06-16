99bf88f21f55283c37ffdb01933d7acd572a80fb
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-16 19:55:35 +0530

    TestArrayIndexOutOfBoundsException

--- src/com/ust/LP3/c1/a3/TestCustomExceptionExample.java
+++ src/com/ust/LP3/c1/a3/TestCustomExceptionExample.java
@@ -1,4 +1,17 @@
 package com.ust.LP3.c1.a3;
 
 public class TestCustomExceptionExample {
+    public static void main(String[] args) {
+        int userInput = -5;
+        try {
+            validateInput(userInput);
+        } catch (CustomException e) {
+            System.out.println("Custom Exception caught: " + e.getMessage());
+        }
+    }
+    private static void validateInput(int input) throws CustomException {
+        if (input < 0) {
+            throw new CustomException("Input cannot be negative");
+        }
+    }
 }
