e8f68c4ffbf664616fb232251ec51ade61a515fd
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-17 16:30:21 +0530

    AppleTree

--- src/com/ust/LP3/c2/runningTaskParallel/AppleTree.java
+++ src/com/ust/LP3/c2/runningTaskParallel/AppleTree.java
@@ -1,4 +1,19 @@
 package com.ust.LP3.c2.runningTaskParallel;
 
 public class AppleTree {
+    public short AppleTree[] newTreeGarden(int size) {
+        AppleTree[] appleTrees = new AppleTree[size];
+        for (int i = 0; i < appleTrees.length; i++) {
+            appleTrees[i] = new AppleTree("\uD83C\uDF33#"+i)
+        }
+        return appleTrees;
+    }
+    private final String treeLabel;
+    private final int numberOfApples;
+
+    public AppleTree(String treeLabel) {
+        this.treeLabel = treeLabel;
+        numberOfApples = 3;
+    }
+
 }
