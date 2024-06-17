1b55234fb35b1d3891a2a8e4dcad50f6144586dc
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-17 16:59:07 +0530

    PickFruitsWithInvokeAll

--- src/com/ust/LP3/c2/runningTaskParallel/PickFruitsWithInvokeAll.java
+++ src/com/ust/LP3/c2/runningTaskParallel/PickFruitsWithInvokeAll.java
@@ -9,9 +9,9 @@ public class PickFruitsWithInvokeAll {
     public static void main(String[] args) {
         AppleTree[] appleTrees = AppleTree.newTreeGarden(6);
 
-        Callable<void> applePicker1 = createApplePicker(appleTrees, 0, 2, "viswa");
-        Callable<void> applePicker2 = createApplePicker(appleTrees, 2, 4, "Axel");
-        Callable<void> applePicker3 = createApplePicker(appleTrees, 4, 6, "anu");
+        Callable<Void> applePicker1 = createApplePicker(appleTrees, 0, 2, "viswa");
+        Callable<Void> applePicker2 = createApplePicker(appleTrees, 2, 4, "Axel");
+        Callable<Void> applePicker3 = createApplePicker(appleTrees, 4, 6, "anu");
 
         ForkJoinPool.commonPool().invokeAll(asList(applePicker1,applePicker2,applePicker2));
         /*ForkJoinPool.invokeAll(asList(applePicker1,applePicker2,applePicker2));// in efficient */
@@ -20,7 +20,7 @@ public class PickFruitsWithInvokeAll {
         System.out.println("All fruits collected");
     }
 
-    public static Callable<void> createApplePicker(AppleTree[] appleTrees, int fromIndexInclusive, int toIndexExclusive, String workerName) {
+    public static Callable<Void> createApplePicker(AppleTree[] appleTrees, int fromIndexInclusive, int toIndexExclusive, String workerName) {
         return () -> {
             return null;
         };
