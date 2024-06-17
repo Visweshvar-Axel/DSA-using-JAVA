81d9a4a280033de072eb9c44b0c0fcc798077299
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-17 17:27:51 +0530

    PickFruitsWithRecursiveTask

--- src/com/ust/LP3/c2/runningTaskParallel/PickFruitsWithRecursiveTask.java
+++ src/com/ust/LP3/c2/runningTaskParallel/PickFruitsWithRecursiveTask.java
@@ -1,4 +1,10 @@
 package com.ust.LP3.c2.runningTaskParallel;
 
+import java.util.concurrent.ForkJoinPool;
+
 public class PickFruitsWithRecursiveTask {
+    public static void main(String[] args) {
+        AppleTree[] appleTrees = AppleTree.newTreeGarden(12);
+        ForkJoinPool pool = ForkJoinPool.commonPool();
+    }
 }
