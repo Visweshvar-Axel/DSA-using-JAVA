d0b229f7ff8a45b83cf62af086874026626645d6
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-17 17:48:04 +0530

    PickFruitsWithRecursiveTask

--- src/com/ust/LP3/c2/runningTaskParallel/PickFruitsWithRecursiveTask.java
+++ src/com/ust/LP3/c2/runningTaskParallel/PickFruitsWithRecursiveTask.java
@@ -1,6 +1,9 @@
 package com.ust.LP3.c2.runningTaskParallel;
 
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.RecursiveTask;
+import java.util.stream.IntStream;
 
 public class PickFruitsWithRecursiveTask {
     public static void main(String[] args) {
@@ -9,16 +12,30 @@ public class PickFruitsWithRecursiveTask {
 
         PickFruitTask task = new
     }
-    public static class PickFruitTask {
+    public static class PickFruitTask extends RecursiveTask<Integer> {
         private final AppleTree[] appleTrees;
         private final int startInclusive;
         private final int endInclusive;
         private final int taskThreadsHold = 4;
-
         public PickFruitTask(AppleTree[] appleTrees, int startInclusive, int endInclusive) {
             this.appleTrees = appleTrees;
             this.startInclusive = startInclusive;
             this.endInclusive = endInclusive;
         }
+        @Override
+        protected Integer compute() {
+            int midpoint = startInclusive + (endInclusive - startInclusive) / 2;
+
+            PickFruitTask leftSum = new PickFruitTask(appleTrees, startInclusive, midpoint);
+            PickFruitTask rightSum = new PickFruitTask(appleTrees, midpoint+1, endInclusive);
+
+            rightSum.fork()
+        }
+        protected Integer deCompute() {
+            return IntStream.rangeClosed(startInclusive,endInclusive)//
+                    .map(i -> appleTrees[i].pickApples())
+                    .sum();
+        }
+
     }
 }
