9562c6973c5672248480882f2697b76225d9af83
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-18 18:14:33 +0530

    RunnableTester

--- src/livesession/L14_16/RunnableTester.java
+++ src/livesession/L14_16/RunnableTester.java
@@ -1,10 +1,10 @@
 package livesession.L14_16;
 
-public class RunnableTester {
-
+public class RunnableTester implements Runnable{
+    @Override
     public void run(){
-        System.out.println("hi i'm from"+Thread.currentThread().getName());
         try {
+        System.out.println("hi i'm from"+Thread.currentThread().getName());
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
@@ -13,7 +13,7 @@ public class RunnableTester {
 
     public static void main(String[] args) {
         for (int i = 0 ; i < 10; i++ ) {
-            Thread hi = new Thread(new ThreadTester());
+            Thread hi = new Thread(new RunnableTester());
             hi.start();
         }
     }
