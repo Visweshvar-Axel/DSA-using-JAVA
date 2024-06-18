0e2a44b479e03280266255951f32f32d71ad1a48
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-19 00:19:39 +0530

    SequentialStreamsTester

--- src/com/ust/LP3/c2/runningTaskParallel/a2/SequentialStreamsTester.java
+++ src/com/ust/LP3/c2/runningTaskParallel/a2/SequentialStreamsTester.java
@@ -6,7 +6,9 @@ import java.util.concurrent.ForkJoinPool;
 public class SequentialStreamsTester {
     private static final ForkJoinPool pool = new ForkJoinPool();
     private static final List<String> ToDo = List.of(
-            "Task: Face wash"
+            "Task: Face wash",
+            "Task: Drink water",
+            "Task: do exercise"
     ) ;
 
 }
