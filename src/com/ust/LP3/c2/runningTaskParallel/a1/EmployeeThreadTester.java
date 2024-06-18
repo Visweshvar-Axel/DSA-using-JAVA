3441d6025eaccea9bafd2fc6b5502fb6b75bced8
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-18 15:47:22 +0530

    EmployeeThreadTester

--- src/com/ust/LP3/c2/runningTaskParallel/a1/EmployeeThreadTester.java
+++ src/com/ust/LP3/c2/runningTaskParallel/a1/EmployeeThreadTester.java
@@ -1,4 +1,14 @@
 package com.ust.LP3.c2.runningTaskParallel.a1;
-class Employee
+class Employee {
+    private String name;
+    private int age;
+    private String department;
+
+    public Employee(String name, int age, String department) {
+        this.name = name;
+        this.age = age;
+        this.department = department;
+    }
+}
 public class EmployeeThreadTester {
 }
