70f75c795ae2b7afcb95ac899e7c0965874565ab
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-18 18:15:43 +0530

    ThreadTester

--- src/livesession/L14_16/ThreadTester.java
+++ src/livesession/L14_16/ThreadTester.java
@@ -2,18 +2,18 @@ package livesession.L14_16;
 
 public class ThreadTester extends Thread{
     public void run(){
-        System.out.println("hi i'm from"+Thread.currentThread().getName());
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        System.out.println("hi i'm from "+Thread.currentThread().getName());
     }
 
     public static void main(String[] args) {
         for (int i = 0 ; i < 10; i++ ) {
             Thread hi = new Thread(new ThreadTester());
             hi.start();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
