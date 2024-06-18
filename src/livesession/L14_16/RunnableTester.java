a05159a212b1dcdf38655aa9df92a752c2e3d623
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-18 18:14:25 +0530

    RunnableTester

--- src/livesession/L14_16/RunnableTester.java
+++ src/livesession/L14_16/RunnableTester.java
@@ -1,2 +1,20 @@
-package livesession.L14_16;public class RunnableTester {
+package livesession.L14_16;
+
+public class RunnableTester {
+
+    public void run(){
+        System.out.println("hi i'm from"+Thread.currentThread().getName());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0 ; i < 10; i++ ) {
+            Thread hi = new Thread(new ThreadTester());
+            hi.start();
+        }
+    }
 }
