ff4d63053d43b0c226a7c5047222783048c9b25c
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-19 02:14:57 +0530

    ThreadsTester

--- src/com/ust/LP3/c2/runningTaskParallel/a5/ThreadsTester.java
+++ src/com/ust/LP3/c2/runningTaskParallel/a5/ThreadsTester.java
@@ -1,4 +1,51 @@
 package com.ust.LP3.c2.runningTaskParallel.a5;
 
+import com.ust.LP3.c2.runningTaskParallel.a4.ReactiveStreamsTester;
+
+import java.util.List;
+import java.util.concurrent.Flow;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.SubmissionPublisher;
+
 public class ThreadsTester {
+    private static final ForkJoinPool pool = new ForkJoinPool();
+    private static final List<String> ToDo = List.of(
+            "Task: Face wash",
+            "Task: Drink water",
+            "Task: do exercise",
+            "Task: Review code"
+    );
+    private static final Object lock = new Object();
+    private static boolean isSecComplete = false;
+    private static boolean isParaComplete = false;
+    public static void main(String[] args) {
+        Thread seqThread = new Thread(() -> SequentialStream(ToDo));
+        Thread paraThread = new Thread(() -> parallelStream(ToDo));
+
+        seqThread.start();
+        paraThread.start();
+
+
+    }
+    public static void SequentialStream(List<String> tasks) {
+        tasks.stream().forEach(task -> {
+            System.out.println("started: "+task);
+            completed(task);
+        });
+    }
+    public static void parallelStream(List<String> tasks) {
+        tasks.stream().forEach(task -> {
+            System.out.println("started: "+task);
+            completed(task);
+        });
+    }
+    private static void completed(String task) {
+        try {
+            Thread.sleep(400);
+            System.out.println(task+" completed ✅");
+        } catch (InterruptedException e) {
+            System.out.println(task+" cant be completed ❌");
+            throw new RuntimeException(e);
+        }
+    }
 }
