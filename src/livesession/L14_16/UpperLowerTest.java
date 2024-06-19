473b30d44634c159cd3643cb0dc9fe9df27e621c
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-19 15:49:03 +0530

    UpperLowerTest

--- src/livesession/L14_16/UpperLowerTest.java
+++ src/livesession/L14_16/UpperLowerTest.java
@@ -4,26 +4,25 @@ import java.io.Reader;
 import java.util.*;
 
 public class UpperLowerTest {
-//    static void upper(List<? extends Objects> l) {
-//       for(Objects i : l){
-//           System.out.println(i);
-//       }
+//    static void upper(List<? extends Number> l) {
+//           System.out.println(l);
 //    }
 //    static void lower(List<? super Integer> l) {
-//       for(Object i : l){
-//           System.out.println(i);
-//       }
+//           System.out.println(l);
 //    }
-    static void upper(List<? extends Number> l) {
-           System.out.println(l);
+    static void upper(List<? extends CharSequence> l) {
+        for (CharSequence cs : l) {
+            System.out.println(cs);
+        }
     }
-    static void lower(List<? super Integer> l) {
-           System.out.println(l);
+    static void lower(List<? super String> l) {
+        l.add("Added String");
+        System.out.println(l);
     }
     public static void main(String[] args) {
-        List<Integer> s = new ArrayList<>();
-        s.add(5);
-        s.add(5);
+        List<String> s = new ArrayList<>();
+        s.add("Viswa");
+        s.add("Axel");
         s.add(5);
         List<Integer> i = Arrays.asList(1,2,3);
         upper(s);
