48765cb19930a06fe48c2bb1f2ea2e6be4141aaf
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-19 18:30:48 +0530

    vectorExample

---
+++ src/livesession/L14_16/vectorExample.java
@@ -0,0 +1,15 @@
+package livesession.L14_16;
+
+import java.util.List;
+import java.util.Vector;
+
+public class vectorExample {
+    public static void main(String[] args) {
+        List<Integer> mainList = new Vector<>();
+        mainList.add(1);
+        mainList.add(2);
+        mainList.add(3);
+        List<Integer> tempList = new Vector<>();
+        tempList.add(5)
+    }
+}
