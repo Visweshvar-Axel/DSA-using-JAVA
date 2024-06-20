fc2774fd26acc39caded9c51b55b99eb76861f59
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-20 22:11:25 +0530

    QueueInBuild

---
+++ src/livesession/L14_16/QueueInBuild.java
@@ -0,0 +1,25 @@
+package livesession.L14_16;
+
+import java.util.Collection;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueInBuild {
+    public static void main(String[] args) {
+        PriorityQueue<Character> chars = new PriorityQueue<>();
+        chars.add('v');
+        chars.add('i');
+        chars.add('s');
+        chars.add('w');
+        chars.add('a');
+        System.out.println(chars.poll());
+        System.out.println(chars.peek());
+        System.out.println(chars);
+        chars.remove();
+        System.out.println(chars);
+        chars.remove();
+        System.out.println(chars);
+        chars.remove();
+        System.out.println(chars);
+    }
+}
