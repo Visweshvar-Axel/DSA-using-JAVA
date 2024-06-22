641075916709acbfc887a5f296c54ba60601c253
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-22 13:30:26 +0530

    BinarySearch

--- src/livesession/L17/BinarySearch.java
+++ src/livesession/L17/BinarySearch.java
@@ -2,6 +2,7 @@ package livesession.L17;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class BinarySearch {
     public static int search(int[] arr,int key){
@@ -16,7 +17,7 @@ public class BinarySearch {
         return -1;
     }
     public static void main(String[] args) {
-        Sca
+        Scanner sc = new Scanner(Sy)
         int[] arr = new int[]{5, 6, 2, 7, 3, 9};
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
