25be4b09d3bb0cccc615821dc2f9921b0c746860
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-22 13:30:08 +0530

    BinarySearch

--- src/livesession/L17/BinarySearch.java
+++ src/livesession/L17/BinarySearch.java
@@ -1,4 +1,29 @@
 package livesession.L17;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class BinarySearch {
+    public static int search(int[] arr,int key){
+        int beg = 0;
+        int end = arr.length-1;
+        while (beg<=end) {
+            int mid = beg + (end - beg) /2;
+            if (arr[mid] == key) return mid;
+            else if (arr[mid] )
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{5, 6, 2, 7, 3, 9};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        int index = search(arr,6);
+        if(index!=-1){
+            System.out.println("the element "+arr[index]+" is found as: "+index);
+        } else {
+            System.out.println("element not found");
+        }
+    }
 }
