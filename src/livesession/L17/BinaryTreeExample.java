69b4f94bef96c8a7f29a2a46354ee0376eb170dc
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-22 11:50:27 +0530

    BinaryTreeExample

---
+++ src/livesession/L17/BinaryTreeExample.java
@@ -0,0 +1,60 @@
+package livesession.L17;
+class Node {
+    int value;
+    Node left, right;
+
+    public Node(int value) {
+        this.value = value;
+        left = right = null;
+    }
+}
+public class BinaryTreeExample {
+    Node root;
+
+    BinaryTreeExample() {
+        root = null;
+    }
+
+    void insert(int value) {
+        root = insertRec(root, value);
+    }
+
+    Node insertRec(Node root, int value) {
+        if (root == null) {
+            root = new Node(value);
+            return root;
+        }
+        if (value < root.value)
+            root.left = insertRec(root.left, value);
+        else if (value > root.value)
+            root.right = insertRec(root.right, value);
+        return root;
+    }
+
+    void inorder() {
+        inorderRec(root);
+    }
+
+    void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.value + " ");
+            inorderRec(root.right);
+        }
+    }
+}
+
+    public static void main(String[] args) {
+        BinaryTreeExample tree = new BinaryTreeExample();
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+
+        tree.inorder();
+    }
+
+}
