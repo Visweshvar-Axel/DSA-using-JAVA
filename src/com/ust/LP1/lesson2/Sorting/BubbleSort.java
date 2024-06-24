e578c6c755aaf2d76c6886c76c0890b217432de4
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-24 23:22:40 +0530

    BubbleSort

--- src/com/ust/LP1/lesson2/Sorting/BubbleSort.java
+++ src/com/ust/LP1/lesson2/Sorting/BubbleSort.java
@@ -19,6 +19,7 @@ public class BubbleSort {
             }
         }
     }
+
     public void sBubbleSort(int[] num){
         for(int i=0;i<num.length;i++){
             for(int j=0;j<num.length-i-1;j++){ //I
