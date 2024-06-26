4bc92bfa5993193eeac0a12caf7ea23d4e297cfb
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-26 23:54:03 +0530

    StringMatch

--- src/com/ust/LP4/a1/StringMatch.java
+++ src/com/ust/LP4/a1/StringMatch.java
@@ -1,7 +1,16 @@
 package com.ust.LP4.a1;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class StringMatch {
     public static String readFiles(String fileName){
+        try {
+            return new String(Files.readAllBytes(Paths.get(fileName)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void main(String[] args) {
