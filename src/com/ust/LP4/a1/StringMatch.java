618f93f6e5979e6ccd839cd06f65e7d1d687a3b5
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-26 23:52:37 +0530

    StringMatch

--- src/com/ust/LP4/a1/StringMatch.java
+++ src/com/ust/LP4/a1/StringMatch.java
@@ -1,8 +1,7 @@
 package com.ust.LP4.a1;
 
 public class StringMatch {
-    public static readFiles(String fileName){
-
+    public static String readFiles(String fileName){
     }
 
     public static void main(String[] args) {
