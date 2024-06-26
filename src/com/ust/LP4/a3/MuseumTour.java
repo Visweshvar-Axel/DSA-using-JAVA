aff379ba76505a810e0f27f13eddac1bf19adfc7
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-27 01:19:10 +0530

    MuseumTour

--- src/com/ust/LP4/a3/MuseumTour.java
+++ src/com/ust/LP4/a3/MuseumTour.java
@@ -1,4 +1,29 @@
 package com.ust.LP4.a3;
 
 public class MuseumTour {
+    public static class Exhibit {
+        private String name;
+        private int id;
+
+        public Exhibit(String name, int id) {
+            this.name = name;
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+    }
 }
