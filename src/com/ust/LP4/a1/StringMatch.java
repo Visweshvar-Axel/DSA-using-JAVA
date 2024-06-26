9ddc164c5b72ad9d8e45c246f0c955ed0de24c21
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-27 00:22:02 +0530

    StringMatch

--- src/com/ust/LP4/a1/StringMatch.java
+++ src/com/ust/LP4/a1/StringMatch.java
@@ -71,20 +71,80 @@ public class StringMatch {
         return false;
     }
 
+    public static int[] computeLPSArray(String pattern) {
+        int length = 0;
+        int i = 1;
+        int patternLength = pattern.length();
+        int[] lps = new int[patternLength];
+        lps[0] = 0;
+
+        while (i < patternLength) {
+            if (pattern.charAt(i) == pattern.charAt(length)) {
+                length++;
+                lps[i] = length;
+                i++;
+            } else {
+                if (length != 0) {
+                    length = lps[length - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+
+    public static boolean KMPSearch(String text, String pattern) {
+        int textLength = text.length();
+        int patternLength = pattern.length();
+        int[] lps = computeLPSArray(pattern);
+        int i = 0;
+        int j = 0;
+
+        while (i < textLength) {
+            if (pattern.charAt(j) == text.charAt(i)) {
+                i++;
+                j++;
+            }
+            if (j == patternLength) {
+                return true;
+            } else if (i < textLength && pattern.charAt(j) != text.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         String innocentText = readFiles("src/com/ust/LP4/a1/innocent_text.txt");
         String hiddenMessage = readFiles("src/com/ust/LP4/a1/message_to_find.txt");
 
         if (naiveSearch(innocentText, hiddenMessage)) {
-            System.out.println("Naive Search: The hidden message was found!");
+            System.out.println("Naive Search: The hidden message \'"+hiddenMessage+"\' was found!");
         } else {
-            System.out.println("Naive Search: The hidden message was not found.");
+            System.out.println("Naive Search: The hidden message \'"+hiddenMessage+"\' was not found.");
         }
 
         if (boyerMooreSearch(innocentText, hiddenMessage)) {
-            System.out.println("Boyer-Moore Search: The hidden message was found!");
+            System.out.println("Boyer-Moore Search: The hidden message \'"+hiddenMessage+"\' was found!");
         } else {
-            System.out.println("Boyer-Moore Search: The hidden message was not found.");
+            System.out.println("Boyer-Moore Search: The hidden message \'"+hiddenMessage+"\' was not found.");
+        }
+
+        if (KMPSearch(innocentText, hiddenMessage)) {
+            System.out.println("KMP Search: The hidden message \'"+hiddenMessage+"\' was found!");
+        } else {
+            System.out.println("KMP Search: The hidden message \'"+hiddenMessage+"\' was not found.");
         }
     }
+    /* *
+    * Naive Search: The hidden message 'visweshvar' was found!
+    * Boyer-Moore Search: The hidden message 'visweshvar' was found!
+    * KMP Search: The hidden message 'visweshvar' was found!
+    * */
 }
