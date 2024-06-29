8ca86a206834204bb14cdfe9055fd702448f371e
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-29 14:36:11 +0530

    Array

--- src/com/ust/LP4/c2/CogsAndPuleys/Array.java
+++ src/com/ust/LP4/c2/CogsAndPuleys/Array.java
@@ -3,7 +3,7 @@ package com.ust.LP4.c2.CogsAndPuleys;
 public class Array {
     public static void printAllElements(int[] anIntArray) {
         for (int i = 0; i < anIntArray.length; i++) {
-            System.out.println(anIntArray[i]+" ");
+            System.out.print(anIntArray[i]+" ");
         }
         System.out.println();
     }
