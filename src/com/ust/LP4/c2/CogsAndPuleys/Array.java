6d36ea28467200b69e2c239d5fee5c86bf20298a
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-29 14:34:19 +0530

    Array

--- src/com/ust/LP4/c2/CogsAndPuleys/Array.java
+++ src/com/ust/LP4/c2/CogsAndPuleys/Array.java
@@ -7,4 +7,25 @@ public class Array {
         }
         System.out.println();
     }
+    public static void insertElementAtIndex(int[] array, int startIndex, int targetIndex){
+        int value = array[startIndex];
+        if(startIndex==targetIndex) return;
+        else if(startIndex < targetIndex) {
+            for (int i = startIndex + 1; i <= targetIndex; i++)
+                array[i - 1] = array[i];
+            array[targetIndex] = value;
+        }
+        else{
+        for(int i=startIndex-1;i>=targetIndex;i--)
+            array[i+1]=array[i];
+        }
+        array[targetIndex] = value;
+    }
+    public static int[] insertElementAtIndex(int[] array,int index, int value) {
+        int[] newArray = new int[array.length+1];
+        for (int i = 0; i < index; i++) {
+            newArray[i] = array[i];
+        }
+        newArray[index] = value;
+    }
 }
