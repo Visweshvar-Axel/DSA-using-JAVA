fdd22fdf29307c0e61e120136c2a34343d22d322
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-29 17:18:22 +0530

    LinkedList

---
+++ src/com/ust/LP4/c2/CogsAndPuleys/LinkedList.java
@@ -0,0 +1,31 @@
+package com.ust.LP4.c2.CogsAndPuleys;
+
+public class LinkedList<E> implements Iterable<E> {
+    int length = 0;
+    Node<E>[] lastModifiedNode;
+    Node<E> first;
+    Node<E> last;
+    public static class Node<E>{
+        protected E value;
+
+        public E getValue() {
+            return value;
+        }
+
+        public void setValue(E value) {
+            this.value = value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        protected Node next;
+    }
+
+    protected Node<E>
+}
