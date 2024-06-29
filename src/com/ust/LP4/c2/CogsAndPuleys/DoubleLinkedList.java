f98b90b4d621eade7cc95abe6cc94e55f4cc1bd9
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-06-29 20:44:18 +0530

    DoubleLinkedList

--- src/com/ust/LP4/c2/CogsAndPuleys/DoubleLinkedList.java
+++ src/com/ust/LP4/c2/CogsAndPuleys/DoubleLinkedList.java
@@ -1,4 +1,33 @@
 package com.ust.LP4.c2.CogsAndPuleys;
 
 public class DoubleLinkedList<E> extends LinkedList<E> {
+    public static class Node<E>{
+        protected E value;
+        protected LinkedList.Node next;
+        protected LinkedList.Node previous;
+
+        public E getValue() {
+            return value;
+        }
+
+        public void setValue(E value) {
+            this.value = value;
+        }
+
+        public LinkedList.Node getNext() {
+            return next;
+        }
+
+        public void setNext(LinkedList.Node next) {
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    ", next=" + next +
+                    '}';
+        }
+    }
 }
