11dca5672169014aea85bf95c1b18879a8de7086
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-01 23:51:05 +0530

    StackImplArray

--- src/com/ust/LP4/c2/AbstractDatatype/StackImplArray.java
+++ src/com/ust/LP4/c2/AbstractDatatype/StackImplArray.java
@@ -3,18 +3,25 @@ package com.ust.LP4.c2.AbstractDatatype;
 public class StackImplArray<E> implements Stack<E> {
     protected E[] array;
     int top = -1;
+
+    public StackImplArray(int size) {
+        array = (E[]) new Object[size];
+    }
+
     @Override
     public void push(E value) {
-
+        if (top == array.length-1) {throw new RuntimeException("No more space in the stack");}
+        array[++top] = value;
     }
 
     @Override
     public E pop() {
-        return null;
+        if (top == -1) {throw new RuntimeException("The stack is empty");}
+        return array[top--];
     }
 
     @Override
     public E peek() {
-        return null;
+        return array[top];
     }
 }
