63006259b1cc7caa15bdb871ba8864ec9af4a075
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-01 16:40:11 +0530

    CircularLinkedList

--- src/com/ust/LP4/c2/CogsAndPuleys/CircularLinkedList.java
+++ src/com/ust/LP4/c2/CogsAndPuleys/CircularLinkedList.java
@@ -11,4 +11,16 @@ public class CircularLinkedList<E> extends LinkedList<E>{
         last = first;
         first = first.next;
     }
+
+    public static void main(String[] args) {
+        CircularLinkedList<Integer> linkedList = new CircularLinkedList<>();
+        linkedList.appendFirst(4);
+        linkedList.appendFirst(1);
+        linkedList.appendFirst(2);
+        linkedList.appendFirst(3);
+        linkedList.appendLast(8);
+        linkedList.appendLast(7);
+        linkedList.appendLast(18);
+        linkedList.appendLast(18);
+    }
 }
