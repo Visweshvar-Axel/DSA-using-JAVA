97ea5587c9145d65e8f7ae43d2e4c4c2667b9ddb
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-01 16:38:45 +0530

    CircularLinkedList

--- src/com/ust/LP4/c2/CogsAndPuleys/CircularLinkedList.java
+++ src/com/ust/LP4/c2/CogsAndPuleys/CircularLinkedList.java
@@ -1,4 +1,14 @@
 package com.ust.LP4.c2.CogsAndPuleys;
 
-public class CircularLinkedList {
+public class CircularLinkedList<E> extends LinkedList<E>{
+    @Override
+    public Node<E> appendFirst(E value) {
+        Node<E> newNode =  super.appendFirst(value);
+        last.next = first;
+        return newNode;
+    }
+    public void rotate() {
+        last = first;
+        first = first.next;
+    }
 }
