78c4b516a52271d82c0f3d521148bab49fdd52fd
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-01 23:34:16 +0530

    StackImplArray

---
+++ src/com/ust/LP4/c2/AbstractDatatype/StackImplArray.java
@@ -0,0 +1,20 @@
+package com.ust.LP4.c2.AbstractDatatype;
+
+public class StackImplArray<E> implements Stack<E> {
+    protected E[] array;
+    int top = -1;
+    @Override
+    public void push(E value) {
+
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+}
