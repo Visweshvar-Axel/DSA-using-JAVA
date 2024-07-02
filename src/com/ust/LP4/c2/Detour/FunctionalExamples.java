c8a3bcdc60db65a4f7ed6d6a94e2dfc299edf4e3
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-02 23:15:04 +0530

    FunctionalExamples

---
+++ src/com/ust/LP4/c2/Detour/FunctionalExamples.java
@@ -0,0 +1,11 @@
+package com.ust.LP4.c2.Detour;
+
+public class FunctionalExamples {
+    public static int sunUnto(int n) {
+        int s=0;
+        for (int i = 0; i <= n; i++) {
+
+
+        }
+    }
+}
