6cfb6fbb97ce2ef3955789c739901fcbe519ca64
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-02 20:13:27 +0530

    QueueImplLinkedList

--- src/com/ust/LP4/c2/AbstractDatatype/QueueImplLinkedList.java
+++ src/com/ust/LP4/c2/AbstractDatatype/QueueImplLinkedList.java
@@ -1,6 +1,25 @@
 package com.ust.LP4.c2.AbstractDatatype;
 
 public class QueueImplLinkedList<E> implements Queue<E> {
+    public class LinkedListNode<E> {
+        private E value;
+        private StackImplLinkedList.LinkedListNode next;
+
+        public LinkedListNode() {
+            value = null;
+            next = null;
+        }
+
+        public LinkedListNode(E value) {
+            this.value = value;
+            next = null;
+        }
+
+        public LinkedListNode(E value, StackImplLinkedList.LinkedListNode next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
     @Override
     public void enqueue(E value) {
 
