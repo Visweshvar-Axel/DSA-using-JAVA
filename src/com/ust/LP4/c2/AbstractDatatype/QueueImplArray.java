c5eaddd0ba6e5ed4358483c00f43c4039ce47460
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-02 20:03:18 +0530

    QueueImplArray

---
+++ src/com/ust/LP4/c2/AbstractDatatype/QueueImplArray.java
@@ -0,0 +1,19 @@
+package com.ust.LP4.c2.AbstractDatatype;
+
+public class QueueImplArray<E> implements Queue<E>{
+
+    @Override
+    public void enqueue(E value) {
+
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+}
