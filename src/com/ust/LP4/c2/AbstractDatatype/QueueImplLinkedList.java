5a53212f24f1a4b6eba36d64643050ac451a77db
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-02 20:13:01 +0530

    QueueImplLinkedList

---
+++ src/com/ust/LP4/c2/AbstractDatatype/QueueImplLinkedList.java
@@ -0,0 +1,18 @@
+package com.ust.LP4.c2.AbstractDatatype;
+
+public class QueueImplLinkedList<E> implements Queue<E> {
+    @Override
+    public void enqueue(E value) {
+
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+}
