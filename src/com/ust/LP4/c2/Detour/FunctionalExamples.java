cae2c52962128b9b254f3d0d85607319a91291ec
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-02 23:23:20 +0530

    FunctionalExamples

--- src/com/ust/LP4/c2/Detour/FunctionalExamples.java
+++ src/com/ust/LP4/c2/Detour/FunctionalExamples.java
@@ -11,4 +11,16 @@ public class FunctionalExamples {
     public static int sunUntoFunctional(int n) {
         return n==0 ? 0 : n + sunUntoFunctional(n-1);
     }
+
+    public static long choose(long n, long r){
+        if (n < r) return 0;
+        if (r == 0 || n == r) return 1;
+        return choose(n-1,r) + choose(n-1,r-1);
+    }
+    public static void main(String[] args) {
+        System.out.println(sunUnto(10));
+        System.out.println(sunUntoFunctional(10));
+
+        System.out.println(choose(10,6));
+    }
 }
