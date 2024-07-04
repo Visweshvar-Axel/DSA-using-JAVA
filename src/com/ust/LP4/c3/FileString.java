8cda4e149ce51bb5fac5146fd590f6ffe3c2fc76
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-04 13:27:58 +0530

    FileString

---
+++ src/com/ust/LP4/c3/FileString.java
@@ -0,0 +1,21 @@
+package com.ust.LP4.c3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class FileString {
+    public static void main(String []args) {
+        FileReader f = null;
+        try {
+            f = new FileReader(new File("team.txt"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        Scanner sc = new Scanner(f);
+        String file = "";
+        while(sc.hasNextLine()) file += sc.nextLine();
+        System.out.println(file);
+    }
+}
