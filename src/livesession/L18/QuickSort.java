8f153cc7bb30f2282136fe23a9f5c6ff7acf3c16
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-04 21:40:03 +0530

    QuickSort

--- src/livesession/L18/QuickSort.java
+++ src/livesession/L18/QuickSort.java
@@ -9,16 +9,7 @@ public class QuickSort {
         if(p<q){
             iter++;
             int j = partition(num,p,q);
-            System.out.println("iter "+iter+":"+Arrays.toString(num));
-            System.out.print("left: ");
-            for (int i = p; i < j-1; i++) {
-                System.out.print(num[i]+", ");
-            }
-            System.out.print("right: ");
-            for (int i = j+1; i < q; i++) {
-                System.out.print(num[i]+", ");
-            }
-            System.out.println();
+
             QuickSort(num, p, j-1);
             System.out.println();
             QuickSort(num, j+1, q);
@@ -26,8 +17,17 @@ public class QuickSort {
     }
     public int partition(int[] num,int p,int q){
         int v = num[(p+q) / 2];
-        System.out.println(Arrays.toString(num));
         System.out.println("pivot : "+v+" at :"+(p+q) / 2);
+        System.out.println("iter "+iter+":"+Arrays.toString(num));
+        System.out.print("left: ");
+        for (int i = p; i < j-1; i++) {
+            System.out.print(num[i]+", ");
+        }
+        System.out.print("right: ");
+        for (int i = j+1; i < q; i++) {
+            System.out.print(num[i]+", ");
+        }
+        System.out.println();
         int i = p;
         int j = q;
         while(i <= j){
@@ -51,7 +51,7 @@ public class QuickSort {
     }
     public static void main(String[] args) {
         QuickSort quickSort = new QuickSort();
-        int[] num = new int[]{/*2,5,7,2,4,2,8,*/1,0,9,3,6};
+        int[] num = new int[]{/*2,5,7,2,4,*/2,8,1,0,9,3,6};
         System.out.println("Before: "+Arrays.toString(num));
         quickSort.QuickSort(num);
         System.out.println("After: "+Arrays.toString(num));
