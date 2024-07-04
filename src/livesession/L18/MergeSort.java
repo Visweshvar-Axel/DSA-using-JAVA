b932cb3b6158981d5dc69c75ceeb48c631eeeea3
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-04 21:59:32 +0530

    MergeSort

--- src/livesession/L18/MergeSort.java
+++ src/livesession/L18/MergeSort.java
@@ -60,9 +60,8 @@ public class MergeSort {
 
                 // Merge the sorted halves
                 merge(arr, l, m, r);
+                return count+1;
             }
-            return count+1;
-
         }
 
         // print method
@@ -75,7 +74,7 @@ public class MergeSort {
 
         // Main method
         public static void main(String args[]) {
-            int arr[] = {2, 34, 21, 54, 1, 234, 5, 6, 0, 12, 11, 13, 5, 6, 7 };
+            int arr[] = {/*2, 34, 21, 54, 1, 234, 5, 6, 0,*/ 12, 11, 13, 5, 6, 7 };
             System.out.println("Given Array");
             printArray(arr);
 
