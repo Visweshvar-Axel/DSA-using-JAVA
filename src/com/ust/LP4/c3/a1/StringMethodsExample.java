dff144572b5f6dd2e50d0f16bd1c9a35ce79eeec
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-05 16:16:01 +0530

    StringMethodsExample

--- src/com/ust/LP4/c3/a1/StringMethodsExample.java
+++ src/com/ust/LP4/c3/a1/StringMethodsExample.java
@@ -2,6 +2,11 @@ package com.ust.LP4.c3.a1;
 
 public class StringMethodsExample {
     public static void main(String[] args) {
-        String s = "Viswaeshvar";
+        String s = "Visweshvar";
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+        System.out.println(s.length());
+        System.out.println(s.charAt(7));
+        System.out.println(s.substring(2,5));
     }
 }
