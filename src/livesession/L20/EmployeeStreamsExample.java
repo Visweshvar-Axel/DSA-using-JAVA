c5d5f4b85963d35addf90c84142ec86a67205270
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-06 11:23:41 +0530

    EmployeeStreamsExample

--- src/livesession/L20/EmployeeStreamsExample.java
+++ src/livesession/L20/EmployeeStreamsExample.java
@@ -12,12 +12,18 @@ public class EmployeeStreamsExample {
         emp.add(new EmployeeExample(103,"janani",22));
         emp.add(new EmployeeExample(104,"jeswin",22));
         emp.add(new EmployeeExample(105,"razeen",22));
+        emp.add(new EmployeeExample(106,"rahul",30));
+        emp.add(new EmployeeExample(107,"anu",17));
         return emp;
     }
     public static void main(String[] args) {
         List<EmployeeExample> emp = getEmployss().stream().filter(obj -> {
             int age = obj.getAge();
             return age >= 18 && age <= 25;}).collect(Collectors.toList());
-        emp.stream().forEach(onj -> System.out.print(obj+", "));
+        emp.stream().forEach(obj -> System.out.print(obj+"\n"));
+
+        System.out.println("name starts with R | r");
+        emp = getEmployss().stream().filter(obj -> obj.getName().toLowerCase().startsWith("r")).collect(Collectors.toList());
+        emp.stream().forEach(obj -> System.out.print(obj+"\n"));
     }
 }
