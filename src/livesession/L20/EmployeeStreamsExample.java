90ef4a4f8b87828b3c3eb8ae57b3bee4ccdb50b5
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-06 11:23:33 +0530

    EmployeeStreamsExample

--- src/livesession/L20/EmployeeStreamsExample.java
+++ src/livesession/L20/EmployeeStreamsExample.java
@@ -1,7 +1,23 @@
 package livesession.L20;
 
-public class EmployeeStreamsExample {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class EmployeeStreamsExample {
+    public static List<EmployeeExample> getEmployss(){
+        List<EmployeeExample> emp = new ArrayList<>();
+        emp.add(new EmployeeExample(101,"viswa",22));
+        emp.add(new EmployeeExample(102,"saran",22));
+        emp.add(new EmployeeExample(103,"janani",22));
+        emp.add(new EmployeeExample(104,"jeswin",22));
+        emp.add(new EmployeeExample(105,"razeen",22));
+        return emp;
+    }
+    public static void main(String[] args) {
+        List<EmployeeExample> emp = getEmployss().stream().filter(obj -> {
+            int age = obj.getAge();
+            return age >= 18 && age <= 25;}).collect(Collectors.toList());
+        emp.stream().forEach(onj -> System.out.print(obj+", "));
     }
 }
