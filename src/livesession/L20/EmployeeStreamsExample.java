48ee967bda03d5c7af36c7dfe461e1efd62fa326
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-06 13:14:20 +0530

    EmployeeStreamsExample

--- src/livesession/L20/EmployeeStreamsExample.java
+++ src/livesession/L20/EmployeeStreamsExample.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class EmployeeStreamsExample {
-    public static List<EmployeeExample> getEmployss(){
+    public static List<EmployeeExample> getEmp(){
         List<EmployeeExample> emp = new ArrayList<>();
         emp.add(new EmployeeExample(101,"viswa",22));
         emp.add(new EmployeeExample(102,"saran",22));
@@ -17,13 +17,21 @@ public class EmployeeStreamsExample {
         return emp;
     }
     public static void main(String[] args) {
-        List<EmployeeExample> emp = getEmployss().stream().filter(obj -> {
+        List<EmployeeExample> emp = getEmp().stream().filter(obj -> {
             int age = obj.getAge();
             return age >= 18 && age <= 25;}).collect(Collectors.toList());
         emp.stream().forEach(obj -> System.out.print(obj+"\n"));
 
         System.out.println("name starts with R | r");
-        emp = getEmployss().stream().filter(obj -> obj.getName().toLowerCase().startsWith("r")).collect(Collectors.toList());
+
+        emp = getEmp().stream().filter(obj -> obj.getName().toLowerCase().startsWith("r")).collect(Collectors.toList());
         emp.stream().forEach(obj -> System.out.print(obj+"\n"));
+
+        List<String> name = getEmp().stream().map(EmployeeExample::getName).collect(Collectors.toList());
+        name.stream().forEach(obj -> System.out.print(obj+", "));
+        System.out.println();
+
+        int sum = getEmp().stream().map(EmployeeExample::getAge).collect(Collectors.toList()).stream().reduce(0,Integer::sum);
+        System.out.println(sum);
     }
 }
