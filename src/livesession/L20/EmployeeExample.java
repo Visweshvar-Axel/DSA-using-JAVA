e543a370dc883685b9f4d068797cf362781cb29b
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-06 11:10:03 +0530

    EmployeeExample

--- src/livesession/L20/EmployeeExample.java
+++ src/livesession/L20/EmployeeExample.java
@@ -11,4 +11,27 @@ public class EmployeeExample {
         this.age = age;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
