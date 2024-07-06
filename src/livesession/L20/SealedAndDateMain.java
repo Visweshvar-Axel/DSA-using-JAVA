c8455ef74aaabd3ce73550384c8b4c0169e0ef5e
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-06 13:14:37 +0530

    SealedAndDateMain

---
+++ src/livesession/L20/SealedAndDateMain.java
@@ -0,0 +1,19 @@
+package livesession.L20;
+
+import java.time.LocalDate;
+
+public class SealedAndDateMain {
+    public static void main(String[] args) {
+        DateExample dt = new DateExample();
+        System.out.println(dt.getNow());
+
+        ExampleSealed fac = new ExampleSealed();
+        System.out.println(fac.fact(5));
+
+        //date and time
+
+        System.out.println(LocalDate.ofYearDay(2001,132));
+        System.out.println(LocalDate.of(2001,5,12));
+        System.out.println(LocalDate.now());
+    }
+}
