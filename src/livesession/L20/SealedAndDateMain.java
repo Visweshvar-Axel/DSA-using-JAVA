6c02ba0e2a8ac056126ecfbd49f1da1298810cce
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-07 11:21:10 +0530

    SealedAndDateMain

--- src/livesession/L20/SealedAndDateMain.java
+++ src/livesession/L20/SealedAndDateMain.java
@@ -10,7 +10,7 @@ public class SealedAndDateMain {
         ExampleSealed fac = new ExampleSealed();
         System.out.println(fac.fact(5));
 
-        //date and time
+        //date and time``
 
         System.out.println(LocalDate.ofYearDay(2001,132));
         System.out.println(LocalDate.of(2001,5,12));
