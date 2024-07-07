5b05a1743f468fd2a9576fcb6d98dfd07d4955cb
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-07 12:35:32 +0530

    AdapterExample

--- src/livesession/L21/AdapterExample.java
+++ src/livesession/L21/AdapterExample.java
@@ -13,6 +13,13 @@ class Volt{
     public void setVolt(int volt) {
         this.volt = volt;
     }
+
+    @Override
+    public String toString() {
+        return "Volt{" +
+                "volt=" + volt +
+                '}';
+    }
 }
 class Socket{
     public static Volt getVolt(){
@@ -30,19 +37,25 @@ class SocketAdapterImpl extends Socket implements SocketAdapter{
         return Socket.getVolt();
     }
 
-//    @Override
-//    public Volt get12Volt() {
-//        return modify();
-//    }
-//
-//    @Override
-//    public Volt get3Volt() {
-//
-//    }
+    @Override
+    public Volt get12Volt() {
+        return modify(Socket.getVolt(),12);
+    }
+
+    @Override
+    public Volt get3Volt() {
+        return modify(Socket.getVolt(),3);
+    }
     private Volt modify(Volt v, int val){
         v.setVolt(val);
         return v;
     }
 }
 public class AdapterExample {
+    public static void main(String[] args) {
+        SocketAdapterImpl socketAdapter = new SocketAdapterImpl();
+        System.out.println(socketAdapter.get120Volt());
+        System.out.println(socketAdapter.get12Volt());
+        System.out.println(socketAdapter.get3Volt());
+    }
 }
