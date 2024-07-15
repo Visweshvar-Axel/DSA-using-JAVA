3dce4a8565af58dafb8481c6f2ec00f5b9206276
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-15 23:14:46 +0530

    LambdaExample

---
+++ src/com/ust/LP6/c1_JAVA_8/LambdaExample.java
@@ -0,0 +1,15 @@
+package com.ust.LP6.c1_JAVA_8;
+
+import java.util.function.IntFunction;
+
+public class LambdaExample {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> System.out.println("viswa"));
+        Thread t2 = new Thread(() -> {
+            System.out.println("viswa");
+            System.out.println("axel");
+        });
+        IntFunction c =  (int a, int b) -> a * b;
+        (a, b) -> a * b;
+    }
+}
