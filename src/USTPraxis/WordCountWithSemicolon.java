0a9e0d6448b0b3be2653f27c83bab4de24889fa7
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-15 15:37:10 +0530

    WordCountWithSemicolon

--- src/USTPraxis/WordCountWithSemicolon.java
+++ src/USTPraxis/WordCountWithSemicolon.java
@@ -5,11 +5,12 @@ import java.util.Arrays;
 public class WordCountWithSemicolon {
     private static int WordCount(String sen, String tar) {
         tar = tar.trim();
+        sen = sen.trim();
         if(sen.length() == 0 || tar.length() == 0) return -1;
         int c = 0;
         sen = sen.replaceAll(";","");
         String[] words = sen.split("\s+");
-        System.out.println(Arrays.toString(words));
+//        System.out.println(Arrays.toString(words));
         for (String word : words) {
             if(word.equals(tar)) c++;
         }
@@ -17,7 +18,7 @@ public class WordCountWithSemicolon {
     }
     public static void main(String[] args) {
         String[][] tests = {
-                {"visw;a ax;el hafueb ksaj;;dhf a;x;e;l a;skdfeh askd;fj easd","axel"},
+                {"visw;a ax;el       hafueb ksaj;;dhf a;x;e;l a;skdfeh askd;fj easd","axel"},
                 {"In today's fast-paced business world, it is crucial to keep up with the ever-changing trends and techniques." +
                         " As a professional, it is important to constantly challenge yourself and strive for growth and improvement in your field." +
                         " This c;an be achieved through continuous learning and staying updated on industry developments." +
@@ -27,7 +28,7 @@ public class WordCountWithSemicolon {
                         " As the saying goes, \"the only cons;tant is change,\" so embrace it with enthusiasm and determination to excel in your profession.","can"},
                 {"visw;a ja;na;ni anu ran ja;na;ni ja;na;ni ja;n;;a;ni ja;;;na;ni ","janani"},
                 {"visw;a ja;na;ni anu ran ja;na;ni ja;na;ni ja;n;;a;ni ja;;;na;ni "," "},
-                {" "," asd"}
+                {"     "," asd"}
         };
         for (String[] test : tests){
             System.out.println(WordCount(test[0],test[1]));
