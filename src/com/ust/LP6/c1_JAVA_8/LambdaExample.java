6895d42a17ecd0417948e12ec5c13106722360aa
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-16 00:07:25 +0530

    LambdaExample

--- src/com/ust/LP6/c1_JAVA_8/LambdaExample.java
+++ src/com/ust/LP6/c1_JAVA_8/LambdaExample.java
@@ -5,13 +5,15 @@ import java.util.function.IntFunction;
 public class LambdaExample {
     public static void main(String[] args) {
         Thread t1 = new Thread(() -> System.out.println("viswa"));
+        Runnable r = () -> System.out.println("viswa");
         Thread t2 = new Thread(() -> {
             System.out.println("viswa");
             System.out.println("axel");
         });
         t1.start();
         t2.start();
-//        IntFunction c =  (int a, int b) -> a * b;
-//        (a, b) -> a * b;
+        r.run();
+        Runnable c =  (int a, int b) -> a * b;
+        (a, b) -> a * b;
     }
 }
