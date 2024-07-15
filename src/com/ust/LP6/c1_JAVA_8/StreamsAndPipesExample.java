e059dd76bfd8606358911a759ab24f52cee71d90
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-16 00:33:07 +0530

    StreamsAndPipesExample

---
+++ src/com/ust/LP6/c1_JAVA_8/StreamsAndPipesExample.java
@@ -0,0 +1,13 @@
+package com.ust.LP6.c1_JAVA_8;
+
+import java.util.stream.Stream;
+
+public class StreamsAndPipesExample {
+    public static void main(String[] args) {
+        Stream.of("viswa","axel","janani","anu","saran")
+                .map(name -> name.toUpperCase())
+                .filter(s -> s.length() > 3)
+                .sorted()
+                .forEach(System.out::println);
+    }
+}
