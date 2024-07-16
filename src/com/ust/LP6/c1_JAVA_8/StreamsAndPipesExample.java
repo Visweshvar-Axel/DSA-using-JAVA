f070d146e6f7175c30a097b09f1e4b802be57863
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-17 01:08:36 +0530

    StreamsAndPipesExample

--- src/com/ust/LP6/c1_JAVA_8/StreamsAndPipesExample.java
+++ src/com/ust/LP6/c1_JAVA_8/StreamsAndPipesExample.java
@@ -49,6 +49,10 @@ public class StreamsAndPipesExample {
             this.exam = exam;
         }
 
+        public String getName() {
+            return name;
+        }
+
         @Override
         public boolean equals(Object o) {
             if (this == o) return true;
