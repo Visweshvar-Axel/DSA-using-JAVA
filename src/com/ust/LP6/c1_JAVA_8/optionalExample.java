e7da448b6a4d44d7e9f90c6c2e967f1890e3baad
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-16 13:02:39 +0530

    optionalExample

--- src/com/ust/LP6/c1_JAVA_8/optionalExample.java
+++ src/com/ust/LP6/c1_JAVA_8/optionalExample.java
@@ -1,5 +1,7 @@
 package com.ust.LP6.c1_JAVA_8;
 
+import java.util.Optional;
+
 class Employee{
     String name;
 
@@ -16,8 +18,9 @@ public class optionalExample {
     public static void main(String[] args) {
 
         Employee e1 = GetEmployeeOfTheMonth();
-        if(e1 != null) System.out.println(e1.);
-        Employee e2 = GetEmployeeOfTheMonthNull();
+        if(e1 != null) System.out.println(e1.getName());
+        Optional<Employee> e2 = Optional.ofNullable(GetEmployeeOfTheMonthNull());
+        e2.ifPresent((emp) -> System.out.println(emp.getName()));
     }
 
     private static Employee GetEmployeeOfTheMonthNull() {
