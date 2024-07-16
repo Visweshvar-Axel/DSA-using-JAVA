85ec98f8d85959b036e4c299271ecde01bf1f3e6
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-16 12:58:50 +0530

    optionalExample

--- src/com/ust/LP6/c1_JAVA_8/optionalExample.java
+++ src/com/ust/LP6/c1_JAVA_8/optionalExample.java
@@ -2,20 +2,29 @@ package com.ust.LP6.c1_JAVA_8;
 
 class Employee{
     String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
 public class optionalExample {
 
     public static void main(String[] args) {
 
-        Employee e = GetEmployeeOfTheMonth();
-        Employee e = GetEmployeeOfTheMonthNull();
+        Employee e1 = GetEmployeeOfTheMonth();
+        if(e1 != null) System.out.println(e1.);
+        Employee e2 = GetEmployeeOfTheMonthNull();
     }
 
     private static Employee GetEmployeeOfTheMonthNull() {
-
+        return null;
     }
 
     private static Employee GetEmployeeOfTheMonth() {
-        return null;
+        return new Employee("viswa");
     }
 }
