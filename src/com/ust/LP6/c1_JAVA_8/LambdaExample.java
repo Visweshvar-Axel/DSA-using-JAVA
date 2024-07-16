bf134b46a87a3e7db340209db09c13dbd8a285e1
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-16 22:21:01 +0530

    LambdaExample

--- src/com/ust/LP6/c1_JAVA_8/LambdaExample.java
+++ src/com/ust/LP6/c1_JAVA_8/LambdaExample.java
@@ -1,5 +1,6 @@
 package com.ust.LP6.c1_JAVA_8;
 
+import java.util.function.Consumer;
 import java.util.function.IntBinaryOperator;
 import java.util.function.IntFunction;
 import java.util.function.Supplier;
@@ -15,11 +16,19 @@ public class LambdaExample {
         t1.start();
         t2.start();
         r.run();
+        Consumer<String> oneArgument = name -> System.out.println("viswa"+name);
+        StringRunnable usingCustom = name -> System.out.println("viswa"+name);
+        oneArgument.accept("AXEL");
+        usingCustom.run("Axel");
         IntBinaryOperator c =  (int a, int b) -> a * b;
         IntBinaryOperator d = (a, b) -> a * b;
         System.out.println(c.applyAsInt(5,5));
         System.out.println(d.applyAsInt(10,10));
         Supplier<String> str = () -> "im viswa";
         System.out.println(str.get());
+    }
+    @FunctionalInterface
+    public interface StringRunnable {
+        void run(String s);
     }
 }
