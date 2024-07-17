54d8794e7fee51037bda9d658e84a266b3af4ca6
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-17 15:14:08 +0530

    FindMax

--- src/com/ust/LP6/c1_JAVA_8/a/FindMax.java
+++ src/com/ust/LP6/c1_JAVA_8/a/FindMax.java
@@ -1,4 +1,14 @@
 package com.ust.LP6.c1_JAVA_8.a;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+
 public class FindMax {
+    public static void main(String[] args) {
+        List<Double> doubles = Arrays.asList(1.5, 2.8, 3.2, 2.0, 5.1);
+        OptionalDouble maxVal = doubles.stream().mapToDouble(Double::doubleValue).max();
+
+        maxVal.ifPresent(v -> System.out.println("Maximum element is:"+v));
+    }
 }
