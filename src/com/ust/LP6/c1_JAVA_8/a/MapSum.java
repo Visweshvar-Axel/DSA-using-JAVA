13d0708c432d02588f346f452216f9c90687770f
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-17 15:06:10 +0530

    MapSum

--- src/com/ust/LP6/c1_JAVA_8/a/MapSum.java
+++ src/com/ust/LP6/c1_JAVA_8/a/MapSum.java
@@ -1,4 +1,12 @@
 package com.ust.LP6.c1_JAVA_8.a;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MapSum {
+    public static void main(String[] args) {
+        List<String> numberStrings = Arrays.asList("1", "2", "3", "4", "5");
+        int sum = numberStrings.stream().mapToInt(Integer::parseInt).sum();
+        System.out.println("sum of all numbers: "+sum);
+    }
 }
