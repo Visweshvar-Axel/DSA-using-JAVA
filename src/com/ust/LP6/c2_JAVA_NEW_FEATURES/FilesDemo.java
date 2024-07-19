08168b9a673313f86e0734462227db1fe8a5dec0
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-20 01:53:41 +0530

    FilesDemo

--- src/com/ust/LP6/c2_JAVA_NEW_FEATURES/FilesDemo.java
+++ src/com/ust/LP6/c2_JAVA_NEW_FEATURES/FilesDemo.java
@@ -20,7 +20,7 @@ public class FilesDemo {
         Path path1 = Path.of("C:\\Users\\VISWA\\IdeaProjects\\JAVA_UST\\src\\com\\ust\\LP6\\c2_JAVA_NEW_FEATURES\\FilesDemolnput1.txt");
 
         System.out.println(Files.isSameFile(path,path));
-        System.out.println(Files.mismatch(path,path));
+        System.out.println(Files.mismatch(path,path1));
 
     }
 }
