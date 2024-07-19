c1a159e056d11ba847a6bc7bb678dce08edd0cec
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-20 01:52:56 +0530

    FilesDemo

--- src/com/ust/LP6/c2_JAVA_NEW_FEATURES/FilesDemo.java
+++ src/com/ust/LP6/c2_JAVA_NEW_FEATURES/FilesDemo.java
@@ -13,5 +13,14 @@ public class FilesDemo {
         //java 11
         System.out.println(Files.readString(path));
 
+        Files.writeString(path, "viswa\naxel");
+        System.out.println(Files.readString(path));
+
+        // java 12
+        Path path1 = Path.of("C:\\Users\\VISWA\\IdeaProjects\\JAVA_UST\\src\\com\\ust\\LP6\\c2_JAVA_NEW_FEATURES\\FilesDemolnput1.txt");
+
+        System.out.println(Files.isSameFile(path,path));
+        System.out.println(Files.mismatch(path,path));
+
     }
 }
