e14ba491a09c28535000c6b0071355d4c3e95989
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-20 01:13:54 +0530

    StringDemo

---
+++ src/com/ust/LP6/c2_JAVA_NEW_FEATURES/StringDemo.java
@@ -0,0 +1,7 @@
+package com.ust.LP6.c2_JAVA_NEW_FEATURES;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        System.out.println("Demo: operations with whitespace in java 11");
+    }
+}
