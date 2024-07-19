f11ec2457cfeee1aa12b5150d356fa7ad654686f
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-20 01:46:11 +0530

    FilesDemo

--- src/com/ust/LP6/c2_JAVA_NEW_FEATURES/FilesDemo.java
+++ src/com/ust/LP6/c2_JAVA_NEW_FEATURES/FilesDemo.java
@@ -1,4 +1,17 @@
 package com.ust.LP6.c2_JAVA_NEW_FEATURES;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FilesDemo {
+    public static void main(String[] args) throws IOException {
+        Path path = Path.of("C:\\Users\\VISWA\\IdeaProjects\\JAVA_UST\\src\\com\\ust\\LP6\\c2_JAVA_NEW_FEATURES\\FilesDemolnput.txt";
+        //old way: java 8
+        Files.lines(path).forEach(line -> System.out.println(line));
+
+        //java 11
+        System.out.println(Files.readString(path));
+
+    }
 }
