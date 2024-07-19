af89e66537ae2f59bf5fa12be94f829efa147a81
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-20 01:23:48 +0530

    StringDemo

--- src/com/ust/LP6/c2_JAVA_NEW_FEATURES/StringDemo.java
+++ src/com/ust/LP6/c2_JAVA_NEW_FEATURES/StringDemo.java
@@ -6,6 +6,22 @@ public class StringDemo {
 
         String str = "     viswa        \u2003";
         System.out.printf("Str: '%s'\n",str);
-        System.out.printf("Str.trim: '%s'\n",str.trim());
+        System.out.printf("Str.trim: '%s'\n",str.trim()); //only return trined less than u 0020
+        System.out.printf("Str: '%s'\n",str.strip());
+        System.out.printf("Str: '%s'\n",str.stripLeading());
+        System.out.printf("Str: '%s'\n",str.stripTrailing());
+
+        String demo = "";
+        System.out.printf("'%s'.isBlank(): %b\n",demo,demo.isBlank());
+
+        demo = "not blank";
+        System.out.printf("'%s'.isBlank(): %b\n",demo,demo.isBlank());
+
+        demo = "          ";
+        System.out.printf("'%s'.isBlank(): %b\n",demo,demo.isBlank());
+
+        demo = "\t    \t\u2003";
+        System.out.printf("'%s'.isBlank(): %b\n",demo,demo.isBlank());
+
     }
 }
