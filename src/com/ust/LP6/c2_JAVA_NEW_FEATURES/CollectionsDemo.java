956849741b8904c9f7b81abc8076db9fa46bb746
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-20 02:33:01 +0530

    CollectionsDemo

--- src/com/ust/LP6/c2_JAVA_NEW_FEATURES/CollectionsDemo.java
+++ src/com/ust/LP6/c2_JAVA_NEW_FEATURES/CollectionsDemo.java
@@ -1,7 +1,9 @@
 package com.ust.LP6.c2_JAVA_NEW_FEATURES;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CollectionsDemo {
     public static void main(String[] args) {
@@ -21,6 +23,26 @@ public class CollectionsDemo {
         // java10.add("asdas"); // unmodifiable list: fails at runtime
         // java10.set(1,"anu"); // unmodifiable list: fails at runtime
 
+        // pre java 9 map modifiable
+        Map<String,String> dogs1 = new HashMap<>();
+        dogs1.put("alex","like jump");
+        dogs1.put("snoop","like swim");
 
+        Map<String,String> dogs2 = new HashMap<>(){
+            {
+                put("alex","like jump");
+                put("snoop","like swim");
+            }
+        };
+
+        // java 9 map modifiable
+        Map<String,String> dogs3 = Map.of(
+                "alex","like jump",
+                "snoop","like swim"
+        );
+
+//        dogs3.put("snoop","like swim"); // fails runtime
+
+        Map<String,String> dog4 = Map.ofEntries(Map.Entry("snoop","like swim"));
     }
 }
