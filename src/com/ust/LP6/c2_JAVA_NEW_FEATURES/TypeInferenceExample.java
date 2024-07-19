d71a301163095120bba7295d6a621b31d7c5da6e
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-19 16:51:34 +0530

    TypeInferenceExample

---
+++ src/com/ust/LP6/c2_JAVA_NEW_FEATURES/TypeInferenceExample.java
@@ -0,0 +1,22 @@
+package com.ust.LP6.c2_JAVA_NEW_FEATURES;
+
+import java.util.List;
+import java.util.Map;
+
+public class TypeInferenceExample {
+    public static void main(String[] args) {
+        String cat = "atiya";
+
+        List<String> catNames= List.of("elle","tim","tom","jack");
+
+        Map<String,List<String>> catWithDescription = Map.of(//
+                "jack", List.of("Super—fluffy "," Sleeps all day long. "),
+                "Ella", List.of("Black Bombay cat.", "Playful, fast, and agile.")
+        );
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
