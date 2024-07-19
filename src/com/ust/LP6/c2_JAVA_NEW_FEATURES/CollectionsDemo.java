4592d754cf31973e684e6fa4c520d512988ee42c
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-20 02:25:33 +0530

    CollectionsDemo

--- src/com/ust/LP6/c2_JAVA_NEW_FEATURES/CollectionsDemo.java
+++ src/com/ust/LP6/c2_JAVA_NEW_FEATURES/CollectionsDemo.java
@@ -1,6 +1,7 @@
 package com.ust.LP6.c2_JAVA_NEW_FEATURES;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class CollectionsDemo {
     public static void main(String[] args) {
@@ -9,5 +10,17 @@ public class CollectionsDemo {
         // preJava9Dogs.add("Cherry"); adding an element to the underlying array: fails at runtime
         pre9.set(1,"anu");
         System.out.println(pre9);
+
+        //java 9
+        var java9 = List.of("viswa","axel");
+        // java9.add("asdas"); // unmodifiable list: fails at runtime
+        // java9.set(1,"anu"); // unmodifiable list: fails at runtime
+
+        //java 10
+        var java10 = List.copyOf(pre9);  // deep copy
+        // java10.add("asdas"); // unmodifiable list: fails at runtime
+        // java10.set(1,"anu"); // unmodifiable list: fails at runtime
+
+
     }
 }
