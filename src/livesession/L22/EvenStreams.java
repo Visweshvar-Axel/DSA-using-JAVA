29bfdc1af218f908b39b721e1e68cf7f86f1a962
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-20 13:26:59 +0530

    EvenStreams

--- src/livesession/L22/EvenStreams.java
+++ src/livesession/L22/EvenStreams.java
@@ -3,12 +3,22 @@ package livesession.L22;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.function.Predicate.not;
+
 public class EvenStreams {
     public static void main(String[] args) {
         var num = List.of(1,2,34,5,54,6,67);
         var even = num.stream()
                 .filter( n -> n%2 == 0)
                 .collect(Collectors.toList());
+        var odd = num.stream()
+                .filter(not(n -> n%2 == 0))
+                .collect(Collectors.toList());
+
+        System.out.println("Even:");
         even.forEach(System.out::println);
+
+        System.out.println("odd:");
+        odd.forEach(System.out::println);
     }
 }
