ef24c30dc29a9cb6d4709f169a96ee1293a67afe
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-20 13:27:19 +0530

    StudentStreams

---
+++ src/livesession/L22/StudentStreams.java
@@ -0,0 +1,24 @@
+package livesession.L22;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Students{
+    String name;
+
+    public Students(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+public class StudentStreams {
+    List<Students> sList = new ArrayList<>();
+
+}
