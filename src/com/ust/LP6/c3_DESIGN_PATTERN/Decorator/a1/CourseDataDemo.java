24dd4f17f3f4f188c5b947c2e8edaad4e67922f2
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-26 04:29:21 +0530

    CourseDataDemo

--- src/com/ust/LP6/c3_DESIGN_PATTERN/Decorator/a1/CourseDataDemo.java
+++ src/com/ust/LP6/c3_DESIGN_PATTERN/Decorator/a1/CourseDataDemo.java
@@ -2,6 +2,10 @@ package com.ust.LP6.c3_DESIGN_PATTERN.Decorator.a1;
 
 public class CourseDataDemo {
     public static void main(String[] args) {
-
+        CourseData courseData1 = CourseData.getInstance();
+        courseData1.getCourseList().stream().forEach(obj -> System.out.println(obj));
+        CourseData courseData2 = CourseData.getInstance();
+        courseData2.getCourseList().stream().forEach(obj -> System.out.println(obj));
+        System.out.println(courseData1.equals(courseData2));
     }
 }
