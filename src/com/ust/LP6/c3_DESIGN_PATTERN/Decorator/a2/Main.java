8a7a2e1947b888c846c67b72f8023202dcda0fee
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-26 04:33:58 +0530

    bill

--- src/com/ust/LP6/c3_DESIGN_PATTERN/Decorator/a2/Main.java
+++ src/com/ust/LP6/c3_DESIGN_PATTERN/Decorator/a2/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Bill bill = new Bill(1, "John Doe");
 
-        Order order1 = new Order(1, "Laptop", 1000.0, 1.0);
-        Order order2 = new Order(2, "Smartphone", 500.0, 2.0);
-
-        bill.addOrder(order1);
-        bill.addOrder(order2);
+//        Order order1 = new Order(1, "Laptop", 1000.0, 1.0);
+//        Order order2 = new Order(2, "Smartphone", 500.0, 2.0);
+//
+//        bill.addOrder(order1);
+//        bill.addOrder(order2);
 
         bill.calculateBillAmount();
         System.out.println("Bill Amount: " + bill.getBillAmount());
