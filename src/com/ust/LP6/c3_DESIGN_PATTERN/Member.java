9d2e9faaf54175c6cb31bf73578547aae6f3520f
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-25 18:32:19 +0530

    Member

---
+++ src/com/ust/LP6/c3_DESIGN_PATTERN/Member.java
@@ -0,0 +1,13 @@
+package com.ust.LP6.c3_DESIGN_PATTERN;
+
+public class Member implements Service{
+    @Override
+    public String getDescription() {
+        return "Member ov the services\n";
+    }
+
+    @Override
+    public int getCost() {
+        return 0;
+    }
+}
