0ad12f90635b174d461a425ecfb080800d6a8b00
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-25 18:40:07 +0530

    ProjectSupport

---
+++ src/com/ust/LP6/c3_DESIGN_PATTERN/ProjectSupport.java
@@ -0,0 +1,17 @@
+package com.ust.LP6.c3_DESIGN_PATTERN;
+
+public class ProjectSupport extends ServiceDecorator{
+    public ProjectSupport(Service service) {
+        super(service);
+    }
+
+    @Override
+    public String getDescription() {
+        return super.service.getDescription()+"ProjectSupport service\n";
+    }
+
+    @Override
+    public int getCost() {
+        return super.service.getCost()+60;
+    }
+}
