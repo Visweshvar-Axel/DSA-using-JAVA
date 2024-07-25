5be9ed0053c01864eaffb70fc4bde7b27c198c30
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-25 18:44:17 +0530

    DemoSubscribeServices

--- src/com/ust/LP6/c3_DESIGN_PATTERN/DemoSubscribeServices.java
+++ src/com/ust/LP6/c3_DESIGN_PATTERN/DemoSubscribeServices.java
@@ -1,4 +1,20 @@
 package com.ust.LP6.c3_DESIGN_PATTERN;
 
 public class DemoSubscribeServices {
+    public static void main(String[] args) {
+        Service guset = new Guest();
+        guset = new OneToOneTraining(guset);
+
+        Service member = new Member();
+        member = new OneToOneTraining(member);
+        member = new ProjectSupport(member);
+
+        System.out.println("Guest details :\n" +
+                guset.getDescription() +
+                "Cost" + guset.getCost());
+
+        System.out.println("member details :\n" +
+                member.getDescription() +
+                "Cost" + member.getCost());
+    }
 }
