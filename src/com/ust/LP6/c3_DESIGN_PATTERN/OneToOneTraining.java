093871f7f6d24e5efebfa2fad911653402686b84
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-25 18:38:21 +0530

    OneToOneTraining

---
+++ src/com/ust/LP6/c3_DESIGN_PATTERN/OneToOneTraining.java
@@ -0,0 +1,17 @@
+package com.ust.LP6.c3_DESIGN_PATTERN;
+
+public class OneToOneTraining extends ServiceDecorator{
+    public OneToOneTraining(Service service) {
+        super(service);
+    }
+
+    @Override
+    public String getDescription() {
+        return super.service.getDescription()+"OneToOneTraining service\n";
+    }
+
+    @Override
+    public int getCost() {
+        return super.service.getCost()+50;
+    }
+}
