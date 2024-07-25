ecb2003c4303c98b57bc13ee677622fdada4b983
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-25 18:33:18 +0530

    Guest

---
+++ src/com/ust/LP6/c3_DESIGN_PATTERN/Guest.java
@@ -0,0 +1,13 @@
+package com.ust.LP6.c3_DESIGN_PATTERN;
+
+public class Guest implements Service{
+    @Override
+    public String getDescription() {
+        return "Guest ov the services\n";
+    }
+
+    @Override
+    public int getCost() {
+        return 0;
+    }
+}
