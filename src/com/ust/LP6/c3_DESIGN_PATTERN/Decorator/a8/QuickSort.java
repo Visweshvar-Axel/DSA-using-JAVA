f6a565e5aa154ca185d4937a21c8263c73bee348
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-27 03:54:52 +0530

    Builder Pattern:

---
+++ src/com/ust/LP6/c3_DESIGN_PATTERN/Decorator/a8/QuickSort.java
@@ -0,0 +1,34 @@
+package com.ust.LP6.c3_DESIGN_PATTERN.Decorator.a8;
+
+public class QuickSort implements SortStrategy {
+    @Override
+    public void sort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+        System.out.println("Quick Sort performed.");
+    }
+
+    private void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pi = partition(array, low, high);
+            quickSort(array, low, pi - 1);
+            quickSort(array, pi + 1, high);
+        }
+    }
+
+    private int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+        return i + 1;
+    }
+}
