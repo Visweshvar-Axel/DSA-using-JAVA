a7837a2cf34c73bf9c330dd1cac0717fc468a8fd
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-27 03:54:04 +0530

    Builder Pattern:

---
+++ src/com/ust/LP6/c3_DESIGN_PATTERN/Decorator/a7/LightTextField.java
@@ -0,0 +1,8 @@
+package com.ust.LP6.c3_DESIGN_PATTERN.Decorator.a7;
+
+public class LightTextField implements TextField {
+    @Override
+    public void render() {
+        System.out.println("Rendering light theme text field.");
+    }
+}
