e6bcf263684c577352ae61f43ebcef6cd8b3c57a
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-28 00:48:24 +0530

    composition

---
+++ src/com/ust/LP6/c6_MODULAR/Composition/Main.java
@@ -0,0 +1,8 @@
+package com.ust.LP6.c6_MODULAR.Composition;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println(person);
+    }
+}
