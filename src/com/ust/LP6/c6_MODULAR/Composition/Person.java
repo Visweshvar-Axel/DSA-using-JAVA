b4db5c9085bcf31dcd5314758810a19a8c8210a3
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-28 00:47:30 +0530

    composition

--- src/com/ust/LP6/c6_MODULAR/Composition/Person.java
+++ src/com/ust/LP6/c6_MODULAR/Composition/Person.java
@@ -7,4 +7,10 @@ public class Person {
     //composition has a
     private Job job;
     private List<Skill> skills = new ArrayList<>();
+
+    public Person() {
+        job = new Job("Software devloper",500000,123124);
+        skills.add(new Skill("java developer","IT",1));
+        skills.add(new Skill("project manager","IT",1));
+    }
 }
