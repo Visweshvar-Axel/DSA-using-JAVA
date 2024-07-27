d8f544a864ae9a071ef372d883a7bb2b5776beee
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-28 00:47:58 +0530

    composition

--- src/com/ust/LP6/c6_MODULAR/Composition/Person.java
+++ src/com/ust/LP6/c6_MODULAR/Composition/Person.java
@@ -13,4 +13,28 @@ public class Person {
         skills.add(new Skill("java developer","IT",1));
         skills.add(new Skill("project manager","IT",1));
     }
+
+    public Job getJob() {
+        return job;
+    }
+
+    public void setJob(Job job) {
+        this.job = job;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "job=" + job +
+                ", skills=" + skills +
+                '}';
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
 }
