d5d68d0eec5f49b62df5c6f2c19d8749bacd9588
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-28 00:44:39 +0530

    composition

--- src/com/ust/LP6/c6_MODULAR/Composition/Job.java
+++ src/com/ust/LP6/c6_MODULAR/Composition/Job.java
@@ -1,4 +1,37 @@
 package com.ust.LP6.c6_MODULAR.Composition;
 
 public class Job {
+    private String role;
+    private long salary;
+    private int id;
+
+    public Job(String role, long salary, int id) {
+        this.role = role;
+        this.salary = salary;
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
