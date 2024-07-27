d81006ace1b91715229f9db93e6e0dad3c43dc44
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-28 00:43:45 +0530

    composition

---
+++ src/com/ust/LP6/c6_MODULAR/Composition/Person.java
@@ -0,0 +1,10 @@
+package com.ust.LP6.c6_MODULAR.Composition;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    //composition has a
+    private Job job;
+    private List<Skill> skills = new ArrayList<>();
+}
