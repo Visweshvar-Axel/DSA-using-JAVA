003dfe6fee5b02071919e875c687ac22d93801eb
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-28 12:13:46 +0530

    nested class

--- src/com/ust/LP6/c6_MODULAR/NestedClasses/Main.java
+++ src/com/ust/LP6/c6_MODULAR/NestedClasses/Main.java
@@ -2,13 +2,18 @@ package com.ust.LP6.c6_MODULAR.NestedClasses;
 
 public class Main {
     public static void main(String[] args) {
-        age.PrintAge();
-    }
-    static Age age = new Age() {
+        Age ageInt = new AgeClass();
+        ageInt.PrintAge();
+        Age age = new Age() {
 
-        @Override
-        public void PrintAge() {
-            System.out.println("My actual age is 26");
-        }
-    };
+            @Override
+            public void PrintAge() {
+                System.out.println("My actual age is 26");
+            }
+        };
+        age.PrintAge();
+
+        CarParts carParts = new CarParts();
+        CarParts.Wheel wheel = new CarParts.Wheel();
+    }
 }
