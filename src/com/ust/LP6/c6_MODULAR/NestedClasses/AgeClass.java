257b6c3f005308b2a1c1d014356bab5402b36208
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-28 12:07:06 +0530

    nested class

---
+++ src/com/ust/LP6/c6_MODULAR/NestedClasses/AgeClass.java
@@ -0,0 +1,8 @@
+package com.ust.LP6.c6_MODULAR.NestedClasses;
+
+public class AgeClass implements Age {
+    @Override
+    public void PrintAge() {
+        System.out.println("My age is "+age);
+    }
+}
