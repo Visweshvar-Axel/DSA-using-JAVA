b90d3c1659d42bc97ab17907522f5d3efa7152b6
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-28 12:03:14 +0530

    nested class

--- src/com/ust/LP6/c6_MODULAR/Composition/PersonNested.java
+++ src/com/ust/LP6/c6_MODULAR/Composition/PersonNested.java
@@ -42,5 +42,90 @@ public class PersonNested {
         private String role;
         private long salary;
         private int id;
+
+        public Job(String role, long salary, int id) {
+            this.role = role;
+            this.salary = salary;
+            this.id = id;
+        }
+
+        public String getRole() {
+            return role;
+        }
+
+        public void setRole(String role) {
+            this.role = role;
+        }
+
+        public long getSalary() {
+            return salary;
+        }
+
+        public void setSalary(long salary) {
+            this.salary = salary;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public String toString() {
+            return "Job{" +
+                    "role='" + role + '\'' +
+                    ", salary=" + salary +
+                    ", id=" + id +
+                    '}';
+        }
     }
+
+    class Skill {
+        private String skillName;
+        private String skillCategory;
+        private int skillRank;
+
+        public Skill(String skillName, String skillCategory, int skillRank) {
+            this.skillName = skillName;
+            this.skillCategory = skillCategory;
+            this.skillRank = skillRank;
+        }
+
+        public String getSkillName() {
+            return skillName;
+        }
+
+        public void setSkillName(String skillName) {
+            this.skillName = skillName;
+        }
+
+        public String getSkillCategory() {
+            return skillCategory;
+        }
+
+        public void setSkillCategory(String skillCategory) {
+            this.skillCategory = skillCategory;
+        }
+
+        public int getSkillRank() {
+            return skillRank;
+        }
+
+        public void setSkillRank(int skillRank) {
+            this.skillRank = skillRank;
+        }
+
+        @Override
+        public String toString() {
+            return "Skill{" +
+                    "skillName='" + skillName + '\'' +
+                    ", skillCategory='" + skillCategory + '\'' +
+                    ", skillRank=" + skillRank +
+                    '}';
+        }
+    }
+
 }
