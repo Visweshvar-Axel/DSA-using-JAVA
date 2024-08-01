89d97a9d7a2f3451ecaa4dea89c1ace17ed97ac4
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-08-01 16:02:46 +0530

    Spring framework

---
+++ SpringDemo/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Product product = context.getBean("Product1",Product.class);
+        System.out.println(product);
+    }
+}
