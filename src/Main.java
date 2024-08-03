7f02fd6f0bd13a44cbfbb88664d5effc1e630449
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-08-04 01:20:12 +0530

    Spring framework

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 //        for (;;) {
 //            System.out.println("viswa");
 //        }
-        do {
-
-        }while (0);
+//        do {
+//
+//        }while (0);
     }
 }
