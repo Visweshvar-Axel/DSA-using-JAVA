49a9f03ffb35fac0092799867346579d1270966e
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-08-07 17:54:36 +0530

    Spring framework web

---
+++ WebDemo/src/main/java/org/example/HelloController.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+//    @RequestMapping(value = "/add")
+//    public ModelAndView addRes(HttpServletResponce){
+//        return null;
+//    }
+}
