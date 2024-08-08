08d9883c5cfb19a488aaac6c97a748976b824914
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-08-09 01:50:57 +0530

    Spring framework web

---
+++ WebDemo/src/main/java/org/example/MyWebInitializer.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
