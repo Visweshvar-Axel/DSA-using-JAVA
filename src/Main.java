f455e46bb99a8a5bcca1c82fe2370dc669348b57
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-08-29 13:27:16 +0530

    main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeMap;
 
 interface Foo {
     String name = "Foo";
@@ -40,6 +41,33 @@ class Class2 extends Class1 {
 
     }
 }
+abstract class Product {
+    public int price;
+
+    Product() {
+        price = 60;
+    }
+
+    abstract public void setPrice(int price);
+    abstract public void getPrice();
+}
+
+class Test extends Product {
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void getPrice() {
+        System.out.println("Price: " + price);
+    }
+
+//    public static void main(String[] args) {
+//        Test obj = new Test();
+//        obj.setPrice(40);
+//        obj.getPrice();
+//    }
+}
+
 public class Main {
     public static void main(String[] args) {
 
@@ -63,6 +91,44 @@ public class Main {
 
         Foo foo = new Bar(); // Line 2
         foo.print(); // Line 3
+
+        TreeMap<Integer, String> stu = new TreeMap<>();
+
+        stu.put(3, "Jack");
+
+        stu.put(6, "Kim");
+
+        stu.put(3, "John");
+
+        stu.put(7, "Amy");
+
+        System.out.println(stu.keySet());
+
+        List<Integer> rollNumberList = new ArrayList<>();
+        rollNumberList.add(1);
+        rollNumberList.add(2);
+        rollNumberList.add(5);
+        rollNumberList.add(4);
+        rollNumberList.add(2);
+        rollNumberList.add(8);
+
+        rollNumberList.sort((Integer roll1, Integer roll2) -> -roll1.compareTo(roll2));
+//        rollNumberList.sort((roll1, roll2) -> {roll1.compareTo(roll2)});
+
+        System.out.println(rollNumberList);
+
+        Test obj = new Test();
+        obj.setPrice(40);
+        obj.getPrice();
+
+        String s1 = "James";
+
+        StringBuilder s2 = new StringBuilder("James");
+        StringBuffer s3 = new StringBuffer("James");
+
+        System.out.println(s1 == s2.toString());
+
+        System.out.println(s1 == s3.toString());
 
 
 //        List<String> str = new ArrayList<String>();
