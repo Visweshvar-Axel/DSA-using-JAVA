f364da4b7de1b96d85fd5aa327b773ecebdc1b21
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-09-07 14:35:24 +0530

    test

--- src/USTPraxis/fundamental/testSort.java
+++ src/USTPraxis/fundamental/testSort.java
@@ -13,9 +13,28 @@ public class testSort {
         Collections.sort(list);
         System.out.println(list);
 
-
-
+        List<Integer[]> work = new ArrayList<>();
+        work.add(new Integer[]{1,2}); 
+        work.add(new Integer[]{5,0}); 
+        work.add(new Integer[]{2,3}); 
+        work.add(new Integer[]{7,1});
         
+        Collections.sort(work,Comparator.comparingInt((Integer[] n)-> n[0]));
+        work.stream().forEach((Integer[] n)-> System.out.println(Arrays.toString(n)));
+        System.out.println();
+        Collections.sort(work,Comparator.comparingInt((Integer[] n)-> n[1]));
+        work.stream().forEach((Integer[] n)-> System.out.println(Arrays.toString(n)));
+        System.out.println();
+        System.out.println();
+        
+        Collections.sort(work,Comparator.comparingInt((Integer[] n)-> n[0]).reversed());
+        work.stream().forEach((Integer[] n)-> System.out.println(Arrays.toString(n)));
+        System.out.println();
+        Collections.sort(work,Comparator.comparingInt((Integer[] n)-> n[1]).reversed());
+        work.stream().forEach((Integer[] n)-> System.out.println(Arrays.toString(n)));
+
+
+
         Queue<Integer> queNor = new PriorityQueue<>();
         queNor.addAll(list);
         System.out.println(queNor.poll());
