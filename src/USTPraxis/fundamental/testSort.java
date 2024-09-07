e115a76b49baefb6504ca06fe692a0c13e61bb3b
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-09-07 14:25:52 +0530

    test

--- src/USTPraxis/fundamental/testSort.java
+++ src/USTPraxis/fundamental/testSort.java
@@ -1,17 +1,30 @@
 package USTPraxis.fundamental;
 
-import java.util.Arrays;
-import java.util.stream.*;
 import java.util.*;
+import java.util.stream.*;
 
 public class testSort {
     public static void main(String[] args) {
-        int[] arr = {12,3,4,45,6};
+        int[] arr = { 121, 123, 3, 3133, 4 };
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
         List<Integer> list = Arrays.stream(arr).boxed().sorted(Collections.reverseOrder()).collect(Collectors.toList());
         System.out.println(list);
         Collections.sort(list);
         System.out.println(list);
+
+
+
+        
+        Queue<Integer> queNor = new PriorityQueue<>();
+        queNor.addAll(list);
+        System.out.println(queNor.poll());
+        System.out.println(queNor.poll());
+        System.out.println(queNor.poll());
+        Queue<Integer> queRev = new PriorityQueue<>(Collections.reverseOrder());
+        queRev.addAll(list);
+        System.out.println(queRev.poll());
+        System.out.println(queRev.poll());
+        System.out.println(queRev.poll());
     }
 }
